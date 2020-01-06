6cee134c8135e958e9b5d602dc5ff9b0cd65e612
Assaf <assafna@post.bgu.ac.il>
2020-01-06 11:30:28 +0200

    added multiprocessing and saving of normalization

--- libs/simulations/save.py
+++ libs/simulations/save.py
@@ -18,3 +18,8 @@ def fibers_densities(_simulation, _time_point, _fibers_densities):
     _simulation_fibers_densities_path = paths.fibers_densities(_simulation)
     _time_point_fibers_densities_path = os.path.join(_simulation_fibers_densities_path, str(_time_point) + '.pkl')
     to_pickle(_fibers_densities, _time_point_fibers_densities_path)
+
+
+def normalization(_simulation, _normalization):
+    _normalization_path = os.path.join(paths.NORMALIZATION, _simulation + '.pkl')
+    to_pickle(_normalization, _normalization_path)
--- methods/simulations/normalization.py
+++ methods/simulations/normalization.py
@@ -1,5 +1,6 @@
 import os
 import time
+from multiprocessing.pool import Pool
 
 import numpy as np
 
@@ -28,17 +29,40 @@ def process_simulation(_simulation, _overwrite=False):
                  list(np.arange(CELLS_ZONE_Y_END, OFFSET_Y_END - ROI_HEIGHT, STEP))
     _time_point_fibers_densities = load.fibers_densities(_simulation, _time_point=0)
 
+    # prepare rois
     _rois = []
     for _offset_x in _offsets_x:
         for _offset_y in _offsets_y:
             _rois.append((_offset_x, _offset_y, _offset_x + ROI_WIDTH, _offset_y + ROI_HEIGHT))
 
+    # compute
     _rois_sums = compute.rois_fibers_densities(_simulation, _time_point=0, _rois=_rois)
 
+    # save fibers densities
     if len(_time_point_fibers_densities) != len(_rois_sums):
         _time_point_fibers_densities.update(_rois_sums)
         save.fibers_densities(_simulation, _time_point=0, _fibers_densities=_time_point_fibers_densities)
 
+    # save
+    _sums = list(_rois_sums.values())
+    _normalization = {
+        'average': np.mean(_sums),
+        'std': np.std(_sums)
+    }
+    save.normalization(_simulation, _normalization)
+
+
+def process_simulations(_simulations, _overwrite=False):
+    _arguments = [(_simulation, _overwrite) for _simulation in _simulations]
+    _p = Pool()
+    _p.starmap(process_simulation, _arguments)
+    _p.close()
+
+
+def process_all_simulations(_overwrite=False):
+    process_simulations(load.structured(), _overwrite)
+
 
 if __name__ == '__main__':
-    process_simulation('3D_1')
+    process_simulation(_simulation='3D_1')
+    # process_all_simulations()
