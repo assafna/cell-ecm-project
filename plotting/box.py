0fb62bc28b5a5e46a9451c607eb6ac31843fef85
Assaf <assafna@post.bgu.ac.il>
2020-01-15 09:51:42 +0200

    added x axis ticks

--- plotting/box.py
+++ plotting/box.py
@@ -27,6 +27,7 @@ def create_plot(_y_array, _names_array, _x_axis_title, _y_axis_title, _title):
 
 def create_group_plot(_x_array, _y_array, _names_array, _x_axis_title, _y_axis_title, _title):
     _data = []
+    _x_ticks = []
     for _x, _y, _name in zip(_x_array, _y_array, _names_array):
         _data.append(
             go.Box(
@@ -35,12 +36,18 @@ def create_group_plot(_x_array, _y_array, _names_array, _x_axis_title, _y_axis_t
                 name=_name
             )
         )
+        for _x_value in _x:
+            _x_ticks.append(_x_value) if _x_value not in _x_ticks else None
 
     _fig = go.Figure(_data)
 
     _fig.update_layout(
         title=_title,
         xaxis_title=_x_axis_title,
+        xaxis=dict(
+            tickmode='array',
+            tickvals=sorted(_x_ticks)
+        ),
         yaxis_title=_y_axis_title,
         boxmode='group'
     )
