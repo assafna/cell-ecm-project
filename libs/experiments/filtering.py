734bfec2f10b7980ba44437a14cb2d341aecf04e
Assaf <assafna@post.bgu.ac.il>
2020-01-24 11:55:12 +0200

    fixed a bug about distance

--- libs/experiments/filtering.py
+++ libs/experiments/filtering.py
@@ -5,15 +5,14 @@ def by_distance(_experiments_tuples, _distance):
     _experiments_tuples_filtered = []
     for _tuple in _experiments_tuples:
         _experiment, _series_id, _group = _tuple
-        _cell_coordinates_tracked_file = 'series_' + str(_series_id) + '.txt'
-        _cell_coordinates_tracked = load.cell_coordinates_tracked_series_file_data(
-            _experiment, _cell_coordinates_tracked_file
-        )
-        _cell_1_id, _cell_2_id = _group.split('_')[1], _group.split('_')[2]
-        _cell_1_coordinates = _cell_coordinates_tracked[int(_cell_1_id)]
-        _cell_2_coordinates = _cell_coordinates_tracked[int(_cell_2_id)]
+        _group_properties = load.group_properties(_experiment, _series_id, _group)
+        _left_cell_coordinates = _group_properties['time_points'][0]['left_cell']['coordinates']
+        _right_cell_coordinates = _group_properties['time_points'][0]['right_cell']['coordinates']
         _cells_distance = compute.cells_distance_in_cell_size(
-            _experiment, _series_id, _cell_1_coordinates, _cell_2_coordinates
+            _experiment=_experiment,
+            _series_id=_series_id,
+            _cell_1_coordinates=[(_left_cell_coordinates['x'], _left_cell_coordinates['y'], _left_cell_coordinates['z'])],
+            _cell_2_coordinates=[(_right_cell_coordinates['x'], _right_cell_coordinates['y'], _right_cell_coordinates['z'])]
         )
         if _distance - 0.5 <= _cells_distance <= _distance + 0.5:
             _experiments_tuples_filtered.append(_tuple)
