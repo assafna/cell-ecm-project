b954f1b0774c3ac8af746ac465b6535b98d77dbe
Assaf <assafna@post.bgu.ac.il>
2020-02-05 23:27:19 +0200

    now extracting new information

--- methods/experiments/extract_data.py
+++ methods/experiments/extract_data.py
@@ -2,28 +2,21 @@ from libs.experiments import load, paths, config, compute
 
 
 def main():
-    for _experiment in config.PAIRS:
-        for _series in paths.folders(paths.structured(_experiment)):
-            _series_id = str(_series.split()[1])
-            for _group in paths.folders(paths.structured(_experiment, _series)):
-                _group_id = _group.split('cells_')[1]
-                for _z_group in paths.folders(paths.structured(_experiment, _series, _group)):
-                    _z_group_id = _z_group.split('cells_')[1]
-                    _time_points_amount = len(
-                        paths.text_files(paths.structured(_experiment, _series, _group, _z_group))
-                    )
-                    _cell_coordinates_tracked_file = 'series_' + _series_id + '.txt'
-                    _cell_coordinates_tracked = load.cell_coordinates_tracked_series_file_data(
-                        _experiment, _cell_coordinates_tracked_file
-                    )
-                    _cell_1_id, _cell_2_id = _group_id.split('_')
-                    _cell_1_coordinates = _cell_coordinates_tracked[int(_cell_1_id) - 1]
-                    _cell_2_coordinates = _cell_coordinates_tracked[int(_cell_2_id) - 1]
-                    _distance = compute.cells_distance_in_cell_size(
-                        _experiment, _series, _cell_1_coordinates, _cell_2_coordinates
-                    )
+    for _tuple in load.experiment_groups_as_tuples('SN41'):
+        _experiment, _series_id, _group = _tuple
+        _cell_coordinates_tracked_file = 'series_' + str(_series_id) + '.txt'
+        _cell_coordinates_tracked = load.cell_coordinates_tracked_series_file_data(
+            _experiment, _cell_coordinates_tracked_file
+        )
+        _, _cell_1_id, _cell_2_id = _group.split('_')
+        _cell_1_coordinates = _cell_coordinates_tracked[int(_cell_1_id)]
+        _cell_2_coordinates = _cell_coordinates_tracked[int(_cell_2_id)]
+        _distance = compute.cells_distance_in_cell_size(
+            _experiment, _series_id, _cell_1_coordinates, _cell_2_coordinates
+        )
+        _properties = load.group_properties(_experiment, _series_id, _group)
 
-                    print(_experiment, _series_id, _group_id, _z_group_id, _time_points_amount, _distance, sep='\t')
+        print(_experiment, _series_id, _group, _distance, _properties['band'], len(_properties['time_points']), sep='\t')
 
 
 if __name__ == '__main__':
