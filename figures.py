f6cf210466eac1af76b930fd8c3e3381bcf11f64
Assaf <assafna@post.bgu.ac.il>
2020-02-05 23:29:38 +0200

    a small change

--- figures.py
+++ figures.py
@@ -1,25 +1,27 @@
-from fibers_density.simulations import fibers_vs_distance, fibers_in_time, different_single_cell_correlations, \
-    insides_vs_outsides_stds, insides_vs_outsides_cell_distances, master_vs_slave, master_vs_slave_cell_distances
+from fibers_density import simulations
+from fibers_density import experiments
 
 
 def figure_1():
-    fibers_vs_distance.main()
+    simulations.fibers_vs_distance.main()
 
 
 def figure_2():
-    fibers_in_time.main()
+    simulations.fibers_in_time.main()
 
 
 def figure_3():
-    different_single_cell_correlations.main()
-    insides_vs_outsides_stds.main()
+    simulations.fibers_vs_derivatives_single_cell.main()
+    simulations.insides_vs_outsides_stds.main()
     # TODO: run when simulations are available
-    insides_vs_outsides_cell_distances.main()
+    simulations.insides_vs_outsides_cell_distances.main()
 
 
 def figure_4():
-    master_vs_slave.main()
-    master_vs_slave_cell_distances.main()
+    # TODO: create a single graph for all derivatives
+    simulations.master_vs_slave.main()
+    experiments.master_vs_slave.main()
+    simulations.master_vs_slave_cell_distances.main()
 
 
 if __name__ == '__main__':
