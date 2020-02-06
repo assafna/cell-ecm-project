a6065c5a5eff8cf0b20b8b71e34bec3c0b5da8e8
Assaf <assafna@post.bgu.ac.il>
2020-02-06 17:13:19 +0200

    added path to experiment SN18

--- methods/experiments/cell_coordinates_tracked.py
+++ methods/experiments/cell_coordinates_tracked.py
@@ -15,15 +15,25 @@ def process_series(_experiment, _series_id, _overwrite=False):
     print('Creating cell coordinates tracked for:', _experiment, 'Series ' + str(_series_id), sep='\t')
     _series_data = load.objects_series_file_data(_experiment, 'Series ' + str(_series_id))
     _cell_coordinates = [[_coordinates] for _coordinates in _series_data[0]]
+    _time_point = 2
     for _tp in _series_data[1:]:
         for _cell_index, _cell in enumerate(_cell_coordinates):
             if _cell[-1] is not None:
                 _previous_cell_x, _previous_cell_y, _previous_cell_z = _cell[-1]
-                _distances = [math.sqrt(
-                    math.pow(_previous_cell_x - _optional_cell_x, 2) +
-                    math.pow(_previous_cell_y - _optional_cell_y, 2) +
-                    math.pow(_previous_cell_z - _optional_cell_z, 2)
-                ) for _optional_cell_x, _optional_cell_y, _optional_cell_z in _tp]
+
+                # fix path for SN18 experiment, where at time point 11 the Z is different, so ignore it
+                if _experiment == 'SN18' and _time_point == 11:
+                    _distances = [math.sqrt(
+                        math.pow(_previous_cell_x - _optional_cell_x, 2) +
+                        math.pow(_previous_cell_y - _optional_cell_y, 2)
+                    ) for _optional_cell_x, _optional_cell_y, _optional_cell_z in _tp]
+                else:
+                    _distances = [math.sqrt(
+                        math.pow(_previous_cell_x - _optional_cell_x, 2) +
+                        math.pow(_previous_cell_y - _optional_cell_y, 2) +
+                        math.pow(_previous_cell_z - _optional_cell_z, 2)
+                    ) for _optional_cell_x, _optional_cell_y, _optional_cell_z in _tp]
+
                 _min_distance_index = int(np.argmin(_distances)) if len(_distances) > 0 else None
                 _cell_coordinates[_cell_index].append(
                     _tp[_min_distance_index] if _min_distance_index is not None and min(
@@ -31,6 +41,7 @@ def process_series(_experiment, _series_id, _overwrite=False):
                 )
             else:
                 _cell_coordinates[_cell_index].append(None)
+        _time_point += 1
 
     save.cell_coordinates_tracked(_experiment, _series_id, _cell_coordinates)
 
