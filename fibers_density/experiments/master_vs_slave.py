4d7a11e97aabf245b61fea900f46df3318ec532c
Assaf <assafna@post.bgu.ac.il>
2020-02-12 17:08:16 +0200

    now with all combinations of master and slave of left and right

--- fibers_density/experiments/master_vs_slave.py
+++ fibers_density/experiments/master_vs_slave.py
@@ -7,19 +7,23 @@ import plotly
 from scipy.stats import wilcoxon
 
 from libs import compute_lib
+from libs.config_lib import CPUS_TO_USE
 from libs.experiments import load, filtering, compute, paths
 from libs.experiments.config import ROI_LENGTH, ROI_WIDTH, ROI_HEIGHT, CELL_DIAMETER_IN_MICRONS
 from methods.experiments import export_video
 from plotting import scatter, save
 
-MINIMUM_TIME_POINTS = 20
+MINIMUM_TIME_POINTS = 240
 OFFSET_X = CELL_DIAMETER_IN_MICRONS * 0
 # TODO: set the offset in y according to the angle in the original Z slices of the cells
-OFFSET_Y = CELL_DIAMETER_IN_MICRONS * 0
+OFFSET_Y = CELL_DIAMETER_IN_MICRONS * 0.5
 OFFSET_Z = CELL_DIAMETER_IN_MICRONS * 0
 DERIVATIVE = 2
-CELLS_DISTANCES = range(4, 16)
+CELLS_DISTANCES = [6, 7, 8, 9]
 DIRECTION = 'inside'
+
+PRINT = True
+SAVE = True
 
 
 def wilcoxon_test(_master_correlations_array, _slave_correlations_array, _name):
@@ -35,20 +39,28 @@ def wilcoxon_test(_master_correlations_array, _slave_correlations_array, _name):
 
 
 def main():
-    _experiments = load.experiment_groups_as_tuples('SN16')
+    _experiments = load.experiments_groups_as_tuples(['SN41'])
     _experiments = filtering.by_distances(_experiments, CELLS_DISTANCES)
 
-    # remove static
-    # _experiments.remove(('SN41', 3, 'noCellsStatic_0_1'))
+    # _experiments.remove(('SN16', 17, 'cells_2_4'))
+    # _experiments.remove(('SN16', 20, 'cells_4_6'))
+    # _experiments.remove(('SN16', 3, 'cells_0_3'))
 
-    _experiments_fake_cells = filtering.by_real_cells(_experiments, _real_cells=False, _static=True)
+    # remove static
+    # _experiments.remove(('SN41', 3, 'static_0_1'))
+
+    # _experiments_fake_cells = filtering.by_real_cells(_experiments, _real_cells=False)
+    # _experiments_fake_cells = filtering.by_static_cells(_experiments_fake_cells, _static=False)
+
+    # _experiments_fake_cells = filtering.by_static_cells(_experiments)
     # _experiments_real_cells = [
-    #     (_tuple[0], _tuple[1], 'cells_' + _tuple[2].split('noCells_')[1]) for _tuple in _experiments_fake_cells
+    #     (_tuple[0], _tuple[1], 'cells_' + _tuple[2].split('fake_')[1]) for _tuple in _experiments_fake_cells
     # ]
 
-    _experiments = _experiments_fake_cells
+    # _experiments = _experiments_fake_cells
+    # _experiments.remove(('SN16', 1, 'fake_0_2'))
 
-    # _experiments = filtering.by_real_cells(_experiments)
+    _experiments = filtering.by_real_cells(_experiments)
 
     _experiments_band = filtering.by_band(_experiments, _band=True)
     _experiments_band = _experiments
@@ -56,7 +68,7 @@ def main():
     _minimum_time_points = MINIMUM_TIME_POINTS
     _experiments = filtering.by_time_points_amount(_experiments, _minimum_time_points)
     print(len(_experiments))
-    # _experiments.remove(('SN16', 13, 'cells_0_1'))
+    _experiments.remove(('SN41', 6, 'cells_1_2'))
     # _experiments.remove(('SN16', 21, 'cells_0_1'))
     # _experiments.remove(('SN16', 1, 'cells_1_5'))
     # _experiments = filtering.by_series_id(_experiments, _series_id=22)
@@ -84,12 +96,13 @@ def main():
     for _tuple in _experiments:
         _experiment, _series, _group = _tuple
         _arguments.append((_experiment, _series, _group, ROI_LENGTH, ROI_WIDTH, ROI_HEIGHT,
-                           OFFSET_X, OFFSET_Y, OFFSET_Z, DIRECTION, _minimum_time_points))
+                           OFFSET_X, OFFSET_Y, OFFSET_Z, DIRECTION, _minimum_time_points,
+                           PRINT, SAVE))
 
         # video
         # export_video.process_group(_experiment, _series, _group)
 
-    _p = Pool()
+    _p = Pool(CPUS_TO_USE)
     _answers = _p.starmap(compute.roi_fibers_density_by_time_pairs, _arguments)
     _p.close()
     for _index in range(len(_arguments)):
@@ -109,9 +122,15 @@ def main():
     for _master_index in range(len(_experiments)):
         _master_tuple = _experiments[_master_index]
         _master_experiment, _master_series, _master_group = _master_tuple
-        _master_left_cell_fibers_densities = _fibers_densities[(_master_experiment, _master_series, _master_group)]['left_cell']
-        _master_right_cell_fibers_densities = _fibers_densities[(_master_experiment, _master_series, _master_group)][
-            'right_cell']
+        _master_left_cell_fibers_densities = \
+            _fibers_densities[(_master_experiment, _master_series, _master_group)]['left_cell']
+        _master_right_cell_fibers_densities = \
+            _fibers_densities[(_master_experiment, _master_series, _master_group)]['right_cell']
+
+        _master_left_cell_fibers_densities_filtered, _master_right_cell_fibers_densities_filtered = \
+            compute.longest_same_indices_shared_in_borders_sub_array(
+                _master_left_cell_fibers_densities, _master_right_cell_fibers_densities
+            )
 
         # take until not 'nan'
         # if any(np.isnan(_master_left_cell_fibers_densities)) or any(np.isnan(_master_right_cell_fibers_densities)):
@@ -119,8 +138,8 @@ def main():
         #     _right_nan_index = np.where(np.isnan(_master_right_cell_fibers_densities))[0][0]
 
         _master_correlation = compute_lib.correlation(
-            compute_lib.derivative(_master_left_cell_fibers_densities, _n=DERIVATIVE),
-            compute_lib.derivative(_master_right_cell_fibers_densities, _n=DERIVATIVE)
+            compute_lib.derivative(_master_left_cell_fibers_densities_filtered, _n=DERIVATIVE),
+            compute_lib.derivative(_master_right_cell_fibers_densities_filtered, _n=DERIVATIVE)
         )
         if _master_tuple in _experiments_band:
             print(_master_tuple, _master_correlation)
@@ -128,27 +147,34 @@ def main():
             if _master_index != _slave_index:
                 _slave_tuple = _experiments[_slave_index]
                 _slave_experiment, _slave_series, _slave_group = _slave_tuple
-                _slave_left_cell_fibers_densities = _fibers_densities[(_slave_experiment, _slave_series,
-                                                                           _slave_group)]['left_cell']
-                _slave_right_cell_fibers_densities = _fibers_densities[(_slave_experiment, _slave_series,
-                                                                           _slave_group)]['right_cell']
-                _slave_correlation = compute_lib.correlation(
-                    compute_lib.derivative(_master_left_cell_fibers_densities, _n=DERIVATIVE),
-                    compute_lib.derivative(_slave_left_cell_fibers_densities, _n=DERIVATIVE)
-                )
+                for _master_cell_id, _slave_cell_id in product(['left_cell', 'right_cell'], ['left_cell', 'right_cell']):
+                    _master_fibers_densities = \
+                        _fibers_densities[(_master_experiment, _master_series, _master_group)][_master_cell_id]
+                    _slave_fibers_densities = \
+                        _fibers_densities[(_slave_experiment, _slave_series, _slave_group)][_slave_cell_id]
 
-                if _master_tuple in _experiments_band and _slave_tuple in _experiments_band:
-                    _band_band_master_correlations_array.append(_master_correlation)
-                    _band_band_slave_correlations_array.append(_slave_correlation)
-                elif _master_tuple in _experiments_band and _slave_tuple not in _experiments_band:
-                    _band_no_band_master_correlations_array.append(_master_correlation)
-                    _band_no_band_slave_correlations_array.append(_slave_correlation)
-                elif _master_tuple not in _experiments_band and _slave_tuple in _experiments_band:
-                    _no_band_band_master_correlations_array.append(_master_correlation)
-                    _no_band_band_slave_correlations_array.append(_slave_correlation)
-                else:
-                    _no_band_no_band_master_correlations_array.append(_master_correlation)
-                    _no_band_no_band_slave_correlations_array.append(_slave_correlation)
+                    _master_fibers_densities_filtered, _slave_fibers_densities_filtered = \
+                        compute.longest_same_indices_shared_in_borders_sub_array(
+                            _master_fibers_densities, _slave_fibers_densities
+                        )
+
+                    _slave_correlation = compute_lib.correlation(
+                        compute_lib.derivative(_master_fibers_densities_filtered, _n=DERIVATIVE),
+                        compute_lib.derivative(_slave_fibers_densities_filtered, _n=DERIVATIVE)
+                    )
+
+                    if _master_tuple in _experiments_band and _slave_tuple in _experiments_band:
+                        _band_band_master_correlations_array.append(_master_correlation)
+                        _band_band_slave_correlations_array.append(_slave_correlation)
+                    elif _master_tuple in _experiments_band and _slave_tuple not in _experiments_band:
+                        _band_no_band_master_correlations_array.append(_master_correlation)
+                        _band_no_band_slave_correlations_array.append(_slave_correlation)
+                    elif _master_tuple not in _experiments_band and _slave_tuple in _experiments_band:
+                        _no_band_band_master_correlations_array.append(_master_correlation)
+                        _no_band_band_slave_correlations_array.append(_slave_correlation)
+                    else:
+                        _no_band_no_band_master_correlations_array.append(_master_correlation)
+                        _no_band_no_band_slave_correlations_array.append(_slave_correlation)
 
     # points plot
     # _fig = scatter.create_plot(
@@ -176,7 +202,7 @@ def main():
     #     _filename='plot_fake_cells'
     # )
 
-    # points plot
+    # # points plot
     # _fig = scatter.create_plot(
     #     _x_array=[_band_band_master_correlations_array],
     #     _y_array=[_band_band_slave_correlations_array],
