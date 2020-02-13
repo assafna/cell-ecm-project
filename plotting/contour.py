659014bb09e7e9a167b30e10a19558f7d7621fbf
Assaf <assafna@post.bgu.ac.il>
2020-02-13 11:05:48 +0200

    updated zmin and zmax

--- plotting/contour.py
+++ plotting/contour.py
@@ -1,7 +1,8 @@
 import plotly.graph_objs as go
 
 
-def create_plot(_x_labels, _y_labels, _z_array, _x_axis_title, _y_axis_title, _color_scale, _show_scale, _title):
+def create_plot(_x_labels, _y_labels, _z_array, _x_axis_title, _y_axis_title, _color_scale=None, _show_scale=True,
+                _zmin=None, _zmax=None, _title=None):
     _fig = go.Figure(
         data=go.Contour(
             x=_x_labels,
@@ -9,8 +10,8 @@ def create_plot(_x_labels, _y_labels, _z_array, _x_axis_title, _y_axis_title, _c
             z=_z_array,
             colorscale=_color_scale,
             showscale=_show_scale,
-            zmin=0,
-            zmax=1
+            zmin=_zmin,
+            zmax=_zmax
         )
     )
 
