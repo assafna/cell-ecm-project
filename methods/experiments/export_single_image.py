588a7365fe829aa0d422a89b020fd71e0675c7b1
Assaf <assafna@post.bgu.ac.il>
2020-02-16 14:10:51 +0200

    nothing special

--- methods/experiments/export_single_image.py
+++ methods/experiments/export_single_image.py
@@ -9,12 +9,12 @@ from libs.experiments.config import FIBERS_CHANNEL_INDEX
 def main():
     _experiment = 'SN41'
     _series_id = 3
-    _group = 'noCellsStatic_0_1'
+    _group = 'static_0_1'
     _x1, _y1, _z1 = 23, 226, 10
     _x2, _y2, _z2 = 228, 226, 10
     _time_point = 34
     _series_image_path = paths.serieses(_experiment, 'series_' + str(_series_id) + '_bc.tif')
-    _image_properties = load.image_properties(_experiment, 'Series ' + str(_series_id))
+    _image_properties = load.image_properties(_experiment, _series_id)
     _series_image = tifffile.imread(_series_image_path)
     _series_image_by_time_points = [
         np.array([_z[FIBERS_CHANNEL_INDEX] for _z in _series_image[_time_point]])
