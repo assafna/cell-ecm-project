cd804550f5239f892d01a562051f233626e0efaf
Assaf <assafna@post.bgu.ac.il>
2020-02-16 14:11:31 +0200

    added for all experiments

--- methods/experiments/extract_data.py
+++ methods/experiments/extract_data.py
@@ -1,23 +1,33 @@
 from libs.experiments import load, paths, config, compute
 
 
-def main():
-    for _tuple in load.experiment_groups_as_tuples('SN41'):
-        _experiment, _series_id, _group = _tuple
-        _cell_coordinates_tracked_file = 'series_' + str(_series_id) + '.txt'
-        _cell_coordinates_tracked = load.cell_coordinates_tracked_series_file_data(
-            _experiment, _cell_coordinates_tracked_file
-        )
-        _, _cell_1_id, _cell_2_id = _group.split('_')
-        _cell_1_coordinates = _cell_coordinates_tracked[int(_cell_1_id)]
-        _cell_2_coordinates = _cell_coordinates_tracked[int(_cell_2_id)]
-        _distance = compute.cells_distance_in_cell_size(
-            _experiment, _series_id, _cell_1_coordinates, _cell_2_coordinates
-        )
-        _properties = load.group_properties(_experiment, _series_id, _group)
+def process_group(_experiment, _series_id, _group):
+    _properties = load.group_properties(_experiment, _series_id, _group)
+    _cell_1_coordinates = _properties['time_points'][0]['left_cell']['coordinates'].values()
+    _cell_2_coordinates = _properties['time_points'][0]['right_cell']['coordinates'].values()
+    _distance = compute.cells_distance_in_cell_size(
+        _experiment, _series_id, [_cell_1_coordinates], [_cell_2_coordinates]
+    )
 
-        print(_experiment, _series_id, _group, _distance, _properties['band'], len(_properties['time_points']), sep='\t')
+    print(_experiment, _series_id, _group, _distance, _properties['band'], _properties['fake'], _properties['static'], len(_properties['time_points']), sep='\t')
+
+
+def process_experiment(_experiment):
+    for _tuple in load.experiment_groups_as_tuples(_experiment):
+        _experiment, _series_id, _group = _tuple
+        process_group(_experiment, _series_id, _group)
+
+
+def process_experiments(_experiments):
+    for _experiment in _experiments:
+        process_experiment(_experiment)
+
+
+def process_all_experiments():
+    # TODO: handle single cell
+    process_experiments(config.PAIRS)
 
 
 if __name__ == '__main__':
-    main()
+    # process_all_experiments()
+    process_experiment('SN44')
