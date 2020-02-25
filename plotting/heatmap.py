351c0cb36af0da885fe5764e87c6c47dd7638018
Assaf <assafna@post.bgu.ac.il>
2020-02-25 19:09:38 +0200

    added annotated heatmap

--- plotting/heatmap.py
+++ plotting/heatmap.py
@@ -1,4 +1,5 @@
 import plotly.graph_objs as go
+import plotly.figure_factory as ff
 
 
 def create_plot(_x_labels, _y_labels, _z_array, _x_axis_title, _y_axis_title, _color_scale=None, _show_scale=True,
@@ -14,6 +15,26 @@ def create_plot(_x_labels, _y_labels, _z_array, _x_axis_title, _y_axis_title, _c
             zmax=_zmax,
             zsmooth=_zsmooth
         )
+    )
+
+    _fig.update_layout(
+        title=_title,
+        xaxis_title=_x_axis_title,
+        yaxis_title=_y_axis_title
+    )
+
+    return _fig
+
+
+def create_annotated_plot(_x_labels, _y_labels, _z_array, _text_array, _x_axis_title, _y_axis_title, _color_scale=None,
+                          _show_scale=True, _title=None):
+    _fig = ff.create_annotated_heatmap(
+        x=list(_x_labels),
+        y=list(_y_labels),
+        z=list(_z_array),
+        annotation_text=list(_text_array),
+        colorscale=_color_scale,
+        showscale=_show_scale
     )
 
     _fig.update_layout(
