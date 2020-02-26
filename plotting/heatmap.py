5d291c9fbf60cb83bbb74344abce622e937673ea
Assaf <assafna@post.bgu.ac.il>
2020-02-26 17:47:17 +0200

    added annotations

--- plotting/heatmap.py
+++ plotting/heatmap.py
@@ -2,8 +2,8 @@ import plotly.graph_objs as go
 import plotly.figure_factory as ff
 
 
-def create_plot(_x_labels, _y_labels, _z_array, _x_axis_title, _y_axis_title, _color_scale=None, _show_scale=True,
-                _zmin=None, _zmax=None, _zsmooth=None, _title=None):
+def create_plot(_x_labels, _y_labels, _z_array, _x_axis_title, _y_axis_title, _annotations_array=None,
+                _color_scale=None, _show_scale=True, _zmin=None, _zmax=None, _zsmooth=None, _title=None):
     _fig = go.Figure(
         data=go.Heatmap(
             x=_x_labels,
@@ -17,25 +17,19 @@ def create_plot(_x_labels, _y_labels, _z_array, _x_axis_title, _y_axis_title, _c
         )
     )
 
-    _fig.update_layout(
-        title=_title,
-        xaxis_title=_x_axis_title,
-        yaxis_title=_y_axis_title
-    )
-
-    return _fig
-
-
-def create_annotated_plot(_x_labels, _y_labels, _z_array, _text_array, _x_axis_title, _y_axis_title, _color_scale=None,
-                          _show_scale=True, _title=None):
-    _fig = ff.create_annotated_heatmap(
-        x=list(_x_labels),
-        y=list(_y_labels),
-        z=list(_z_array),
-        annotation_text=list(_text_array),
-        colorscale=_color_scale,
-        showscale=_show_scale
-    )
+    if _annotations_array is not None:
+        _fig.update_layout(annotations=[
+            go.layout.Annotation(
+                text=_annotation['text'],
+                showarrow=_annotation['show_arrow'],
+                x=_annotation['x'],
+                y=_annotation['y'],
+                font=dict(
+                    size=_annotation['font']['size'],
+                    color=_annotation['font']['color']
+                ) if 'font' in _annotation else None
+            ) for _annotation in _annotations_array
+        ])
 
     _fig.update_layout(
         title=_title,
