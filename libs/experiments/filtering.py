4cd75e0e2823998727eff17b39b68ee1e8439589
Assaf <assafna@post.bgu.ac.il>
2020-03-01 13:51:04 +0200

    added ability to compute cells distance by specific time point

--- libs/experiments/filtering.py
+++ libs/experiments/filtering.py
@@ -1,13 +1,15 @@
 from libs.experiments import load, compute, paths
 
 
-def by_distance(_experiments_tuples, _distance):
+def by_distance(_experiments_tuples, _distance, _time_point=0):
     _experiments_tuples_filtered = []
     for _tuple in _experiments_tuples:
         _experiment, _series_id, _group = _tuple
         _group_properties = load.group_properties(_experiment, _series_id, _group)
-        _left_cell_coordinates = _group_properties['time_points'][0]['left_cell']['coordinates']
-        _right_cell_coordinates = _group_properties['time_points'][0]['right_cell']['coordinates']
+        if len(_group_properties['time_points']) <= _time_point:
+            continue
+        _left_cell_coordinates = _group_properties['time_points'][_time_point]['left_cell']['coordinates']
+        _right_cell_coordinates = _group_properties['time_points'][_time_point]['right_cell']['coordinates']
         _cells_distance = compute.cells_distance_in_cell_size(
             _experiment=_experiment,
             _series_id=_series_id,
