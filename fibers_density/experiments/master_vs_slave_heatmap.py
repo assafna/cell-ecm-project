b0ab62fef31e8006c5e81df19ad96e8215af0a29
Assaf <assafna@post.bgu.ac.il>
2020-03-04 19:11:13 +0200

    now even faster haha

--- fibers_density/experiments/master_vs_slave_heatmap.py
+++ fibers_density/experiments/master_vs_slave_heatmap.py
@@ -18,13 +18,13 @@ from plotting import scatter, save, heatmap, contour
 
 EXPERIMENTS = ['SN16']
 EXPERIMENTS_STR = '_'.join(EXPERIMENTS)
-REAL_CELLS = False
+REAL_CELLS = True
 STATIC = False
 BAND = True
-DOMAIN_ABSOLUTE_VALUE = 10
+DOMAIN_ABSOLUTE_VALUE = 5
 VALUES_BY_CELL_DIAMETER = [
     round(_value, 10) for _value in
-    np.arange(start=0, stop=DOMAIN_ABSOLUTE_VALUE + 0.1, step=0.1) - DOMAIN_ABSOLUTE_VALUE / 2]
+    np.arange(start=0, stop=DOMAIN_ABSOLUTE_VALUE * 2 + 0.1, step=0.1) - DOMAIN_ABSOLUTE_VALUE]
 OFFSET_X = 0
 DERIVATIVE = 2
 CELLS_DISTANCES = [6, 7, 8, 9]
@@ -35,6 +35,106 @@ MINIMUM_CORRELATION_TIME_POINTS = {
     'SN41': 50,
     'SN44': 50
 }
+
+
+def compute_data(_arguments):
+    _experiments_fibers_densities, _experiments, _offset_y_index, _offset_y, _offset_z_index, _offset_z = _arguments
+    _master_correlations_array = []
+    _slave_correlations_array = []
+    for _master_index in range(len(_experiments)):
+        _master_tuple = _experiments[_master_index]
+        _master_experiment, _master_series, _master_group = _master_tuple
+
+        _master_left_cell_fibers_densities = \
+            _experiments_fibers_densities[
+                (_master_experiment, _master_series, _master_group, _offset_y, _offset_z, 'left_cell')]
+        _master_right_cell_fibers_densities = \
+            _experiments_fibers_densities[
+                (_master_experiment, _master_series, _master_group, _offset_y, _offset_z, 'right_cell')]
+
+        _master_properties = load.group_properties(_master_experiment, _master_series, _master_group)
+        _master_left_cell_fibers_densities = compute.remove_blacklist(
+            _master_experiment, _master_series, _master_properties['cells_ids']['left_cell'],
+            _master_left_cell_fibers_densities)
+        _master_right_cell_fibers_densities = compute.remove_blacklist(
+            _master_experiment, _master_series, _master_properties['cells_ids']['right_cell'],
+            _master_right_cell_fibers_densities)
+
+        _master_left_cell_fibers_densities_filtered, _master_right_cell_fibers_densities_filtered = \
+            compute.longest_same_indices_shared_in_borders_sub_array(
+                _master_left_cell_fibers_densities, _master_right_cell_fibers_densities
+            )
+
+        # ignore small arrays
+        if len(_master_left_cell_fibers_densities_filtered) < MINIMUM_CORRELATION_TIME_POINTS[_master_experiment]:
+            continue
+
+        _master_correlation = compute_lib.correlation(
+            compute_lib.derivative(_master_left_cell_fibers_densities_filtered, _n=DERIVATIVE),
+            compute_lib.derivative(_master_right_cell_fibers_densities_filtered, _n=DERIVATIVE)
+        )
+        for _slave_index in range(len(_experiments)):
+            if _master_index != _slave_index:
+                _slave_tuple = _experiments[_slave_index]
+                _slave_experiment, _slave_series, _slave_group = _slave_tuple
+
+                for _master_cell_id, _slave_cell_id in product(['left_cell', 'right_cell'],
+                                                               ['left_cell', 'right_cell']):
+                    _master_fibers_densities = \
+                        _experiments_fibers_densities[
+                            (_master_experiment, _master_series, _master_group, _offset_y, _offset_z,
+                             _master_cell_id)]
+                    _slave_fibers_densities = \
+                        _experiments_fibers_densities[
+                            (_slave_experiment, _slave_series, _slave_group, _offset_y, _offset_z, _slave_cell_id)]
+
+                    _slave_properties = load.group_properties(_slave_experiment, _slave_series, _slave_group)
+                    _master_fibers_densities = compute.remove_blacklist(
+                        _master_experiment, _master_series, _master_properties['cells_ids'][_master_cell_id],
+                        _master_fibers_densities)
+                    _slave_fibers_densities = compute.remove_blacklist(
+                        _slave_experiment, _slave_series, _slave_properties['cells_ids'][_slave_cell_id],
+                        _slave_fibers_densities)
+
+                    _master_fibers_densities_filtered, _slave_fibers_densities_filtered = \
+                        compute.longest_same_indices_shared_in_borders_sub_array(
+                            _master_fibers_densities, _slave_fibers_densities
+                        )
+
+                    # ignore small arrays
+                    if len(_master_fibers_densities_filtered) < MINIMUM_CORRELATION_TIME_POINTS[_slave_experiment]:
+                        continue
+
+                    _slave_correlation = compute_lib.correlation(
+                        compute_lib.derivative(_master_fibers_densities_filtered, _n=DERIVATIVE),
+                        compute_lib.derivative(_slave_fibers_densities_filtered, _n=DERIVATIVE)
+                    )
+                    _master_correlations_array.append(_master_correlation)
+                    _slave_correlations_array.append(_slave_correlation)
+
+    # compute percentages
+    _annotation = None
+    _master_minus_slave = np.array(_master_correlations_array) - np.array(_slave_correlations_array)
+    _master_count = len(_master_minus_slave[_master_minus_slave > 0])
+    if len(_master_minus_slave) > 0:
+        _master_percentages = round(_master_count / len(_master_minus_slave), 10)
+        _wilcoxon = wilcoxon(_master_minus_slave)
+        _p_value = _wilcoxon[1]
+        if _p_value > 0.05:
+            _annotation = {
+                'text': 'X',
+                'show_arrow': False,
+                'x': _offset_y,
+                'y': _offset_z,
+                'font': {
+                    'size': 6,
+                    'color': 'red'
+                }
+            }
+    else:
+        _master_percentages = None
+
+    return _offset_z_index, _offset_y_index, _master_percentages, _annotation
 
 
 def main():
@@ -68,117 +168,27 @@ def main():
         compute.rois(_arguments, _keys=['experiment', 'series_id', 'group', 'offset_y', 'offset_z', 'cell_id'])
     _fibers_densities = compute.fibers_densities(_rois_to_compute)
 
+    _experiments_fibers_densities = {}
+    for _key in tqdm(_rois_dictionary):
+        _experiments_fibers_densities[_key] = [_fibers_densities[_tuple] for _tuple in _rois_dictionary[_key]]
+
+    _arguments = []
+    for (_offset_y_index, _offset_y), (_offset_z_index, _offset_z) in \
+            product(enumerate(VALUES_BY_CELL_DIAMETER), enumerate(VALUES_BY_CELL_DIAMETER)):
+        _arguments.append(
+            (_experiments_fibers_densities, _experiments, _offset_y_index, _offset_y, _offset_z_index, _offset_z))
+
     _z_array = np.zeros(shape=(len(VALUES_BY_CELL_DIAMETER), len(VALUES_BY_CELL_DIAMETER)))
     _annotations_array = []
-    for (_offset_y_index, _offset_y), (_offset_z_index, _offset_z) in \
-            tqdm(product(enumerate(VALUES_BY_CELL_DIAMETER), enumerate(VALUES_BY_CELL_DIAMETER)),
-                 total=len(list(product(enumerate(VALUES_BY_CELL_DIAMETER), enumerate(VALUES_BY_CELL_DIAMETER)))),
-                 desc='Offsets Loop'):
-
-        _master_correlations_array = []
-        _slave_correlations_array = []
-        for _master_index in range(len(_experiments)):
-            _master_tuple = _experiments[_master_index]
-            _master_experiment, _master_series, _master_group = _master_tuple
-
-            _master_left_cell_fibers_densities = \
-                [_fibers_densities[_tuple] for _tuple in
-                 _rois_dictionary[
-                     (_master_experiment, _master_series, _master_group, _offset_y, _offset_z, 'left_cell')]]
-            _master_right_cell_fibers_densities = \
-                [_fibers_densities[_tuple] for _tuple in
-                 _rois_dictionary[
-                     (_master_experiment, _master_series, _master_group, _offset_y, _offset_z, 'right_cell')]]
-
-            _master_properties = load.group_properties(_master_experiment, _master_series, _master_group)
-            _master_left_cell_fibers_densities = compute.remove_blacklist(
-                _master_experiment, _master_series, _master_properties['cells_ids']['left_cell'],
-                _master_left_cell_fibers_densities)
-            _master_right_cell_fibers_densities = compute.remove_blacklist(
-                _master_experiment, _master_series, _master_properties['cells_ids']['right_cell'],
-                _master_right_cell_fibers_densities)
-
-            _master_left_cell_fibers_densities_filtered, _master_right_cell_fibers_densities_filtered = \
-                compute.longest_same_indices_shared_in_borders_sub_array(
-                    _master_left_cell_fibers_densities, _master_right_cell_fibers_densities
-                )
-
-            # ignore small arrays
-            if len(_master_left_cell_fibers_densities_filtered) < MINIMUM_CORRELATION_TIME_POINTS[_master_experiment]:
-                continue
-
-            _master_correlation = compute_lib.correlation(
-                compute_lib.derivative(_master_left_cell_fibers_densities_filtered, _n=DERIVATIVE),
-                compute_lib.derivative(_master_right_cell_fibers_densities_filtered, _n=DERIVATIVE)
-            )
-            for _slave_index in range(len(_experiments)):
-                if _master_index != _slave_index:
-                    _slave_tuple = _experiments[_slave_index]
-                    _slave_experiment, _slave_series, _slave_group = _slave_tuple
-
-                    for _master_cell_id, _slave_cell_id in product(['left_cell', 'right_cell'],
-                                                                   ['left_cell', 'right_cell']):
-                        _master_fibers_densities = \
-                            [_fibers_densities[_tuple] for _tuple in
-                             _rois_dictionary[
-                                 (_master_experiment, _master_series, _master_group, _offset_y, _offset_z,
-                                  _master_cell_id)]]
-                        _slave_fibers_densities = \
-                            [_fibers_densities[_tuple] for _tuple in
-                             _rois_dictionary[
-                                 (_slave_experiment, _slave_series, _slave_group, _offset_y, _offset_z,
-                                  _slave_cell_id)]]
-
-                        _slave_properties = load.group_properties(_slave_experiment, _slave_series, _slave_group)
-                        _master_fibers_densities = compute.remove_blacklist(
-                            _master_experiment, _master_series, _master_properties['cells_ids'][_master_cell_id],
-                            _master_fibers_densities)
-                        _slave_fibers_densities = compute.remove_blacklist(
-                            _slave_experiment, _slave_series, _slave_properties['cells_ids'][_slave_cell_id],
-                            _slave_fibers_densities)
-
-                        _master_fibers_densities_filtered, _slave_fibers_densities_filtered = \
-                            compute.longest_same_indices_shared_in_borders_sub_array(
-                                _master_fibers_densities, _slave_fibers_densities
-                            )
-
-                        # ignore small arrays
-                        if len(_master_fibers_densities_filtered) < MINIMUM_CORRELATION_TIME_POINTS[_slave_experiment]:
-                            continue
-
-                        _slave_correlation = compute_lib.correlation(
-                            compute_lib.derivative(_master_fibers_densities_filtered, _n=DERIVATIVE),
-                            compute_lib.derivative(_slave_fibers_densities_filtered, _n=DERIVATIVE)
-                        )
-                        _master_correlations_array.append(_master_correlation)
-                        _slave_correlations_array.append(_slave_correlation)
-
-        # compute percentages
-        _master_minus_slave = np.array(_master_correlations_array) - np.array(_slave_correlations_array)
-        _master_count = len(_master_minus_slave[_master_minus_slave > 0])
-        if len(_master_minus_slave) > 0:
-            _master_percentages = round(_master_count / len(_master_minus_slave), 10)
-            _wilcoxon = wilcoxon(_master_minus_slave)
-            _p_value = _wilcoxon[1]
-            if _p_value > 0.05:
-                _annotations_array.append({
-                    'text': 'X',
-                    'show_arrow': False,
-                    'x': _offset_y,
-                    'y': _offset_z,
-                    'font': {
-                        'size': 6,
-                        'color': 'red'
-                    }
-                })
-            # print('z:', _offset_y, 'xy:', _offset_z, 'Master:', _master_percentages, 'N:', len(_master_minus_slave),
-            #       'Wilcoxon:', compute_lib.p_value_text(_p_value), sep='\t')
-        else:
-            _master_percentages = None
-            _p_value = None
-            # print('z:', _offset_y, 'xy:', _offset_z, 'Master:', _master_percentages, 'N:', 0, sep='\t')
-
-        _z_array[_offset_z_index, _offset_y_index] = _master_percentages
+    with Pool(CPUS_TO_USE) as _p:
+        for _answer in tqdm(_p.imap_unordered(compute_data, _arguments), total=len(_arguments),
+                            desc='Computing Heatmap'):
+            _offset_z_index, _offset_y_index, _master_percentages, _annotation = _answer
+            _z_array[_offset_z_index, _offset_y_index] = _master_percentages
+            if _annotation is not None:
+                _annotations_array.append(_annotation)
+        _p.close()
+        _p.join()
 
     # plot
     _fig = heatmap.create_plot(
