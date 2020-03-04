185463715cd3e4e249c9e8977386518b56488c17
Assaf <assafna@post.bgu.ac.il>
2020-03-04 13:10:47 +0200

    faster multiprocessing

--- fibers_density/fibers_vs_distance_single_cells.py
+++ fibers_density/fibers_vs_distance_single_cells.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 from fibers_density.fibers_vs_distance_pairs import EXPERIMENTS_OFFSETS_X, SIMULATIONS_OFFSETS_X
 from libs import compute_lib, paths_lib
 from libs.config_lib import CPUS_TO_USE
-from libs.experiments import compute as experiments_compute
+from libs.experiments import compute as experiments_compute, compute
 from libs.experiments import config as experiments_config
 from libs.experiments import filtering as experiments_filtering
 from libs.experiments import load as experiments_load
@@ -31,37 +31,6 @@ OUT_OF_BOUNDARIES = False
 
 # simulations
 SIMULATIONS_TIME_POINT = 50
-
-
-def compute_experiments_fibers_densities(_experiments):
-    _arguments = []
-    for _tuple in _experiments:
-        _experiment, _series_id, _group = _tuple
-        for _offset_x, _direction in product(OFFSETS_X, ['left', 'right']):
-            _arguments.append({
-                'experiment': _experiment,
-                'series_id': _series_id,
-                'group': _group,
-                'length_x': experiments_config.ROI_LENGTH,
-                'length_y': experiments_config.ROI_HEIGHT,
-                'length_z': experiments_config.ROI_WIDTH,
-                'offset_x': _offset_x,
-                'offset_y': OFFSET_Y,
-                'offset_z': OFFSET_Z,
-                'cell_id': 'cell',
-                'direction': _direction,
-                'time_point': EXPERIMENTS_TIME_POINT - 1
-            })
-
-    _fibers_densities = {}
-    with Pool(CPUS_TO_USE) as _p:
-        for _keys, _value in tqdm(
-                _p.imap_unordered(experiments_compute.roi_fibers_density_time_point, _arguments), total=len(_arguments)):
-            _fibers_densities[(_keys['experiment'], _keys['series_id'], _keys['group'], _keys['offset_x'], _keys['direction'])] = _value
-        _p.close()
-        _p.join()
-
-    return _fibers_densities
 
 
 def compute_simulations_fibers_densities(_simulations):
@@ -87,7 +56,7 @@ def compute_simulations_fibers_densities(_simulations):
     with Pool(CPUS_TO_USE) as _p:
         for _keys, _value in tqdm(
                 _p.imap_unordered(simulations_compute.roi_fibers_density_time_point, _arguments),
-                total=len(_arguments)):
+                total=len(_arguments), desc='Computing Rois & Fibers Densities'):
             if _keys['direction'] in ['up', 'down']:
                 _fibers_densities[(_keys['simulation'], _keys['offset_y'], _keys['direction'])] = _value
             else:
@@ -104,12 +73,32 @@ def main():
     _experiments = experiments_load.experiments_groups_as_tuples(experiments_config.SINGLE_CELL)
     _experiments = experiments_filtering.by_time_points_amount(_experiments, EXPERIMENTS_TIME_POINT)
 
-    _fibers_densities = compute_experiments_fibers_densities(_experiments)
+    _arguments = []
+    for _tuple in _experiments:
+        _experiment, _series_id, _group = _tuple
+        for _offset_x, _direction in product(OFFSETS_X, ['left', 'right']):
+            _arguments.append({
+                'experiment': _experiment,
+                'series_id': _series_id,
+                'group': _group,
+                'length_x': experiments_config.ROI_LENGTH,
+                'length_y': experiments_config.ROI_HEIGHT,
+                'length_z': experiments_config.ROI_WIDTH,
+                'offset_x': _offset_x,
+                'offset_y': OFFSET_Y,
+                'offset_z': OFFSET_Z,
+                'cell_id': 'cell',
+                'direction': _direction,
+                'time_point': EXPERIMENTS_TIME_POINT - 1
+            })
+
+    _rois = compute.rois(_arguments)
+    _fibers_densities = compute.fibers_densities(_rois)
 
     _experiments = experiments_organize.by_single_cell_id(_experiments)
 
     _experiments_fibers_densities = [[] for _i in range(len(OFFSETS_X))]
-    for _tuple in _experiments:
+    for _tuple in tqdm(_experiments, desc='Experiments Loop'):
         _experiment, _series_id, _group = _tuple
         _offset_index = 0
         _normalization = experiments_load.normalization_series_file_data(_experiment, 'Series ' + str(_series_id))
@@ -119,7 +108,24 @@ def main():
             for _cell_tuple in _experiments[_tuple]:
                 _, _, _group = _cell_tuple
                 for _direction in ['left', 'right']:
-                    _fibers_density = _fibers_densities[(_experiment, _series_id, _group, _offset_x, _direction)]
+
+                    _arguments = {
+                        'experiment': _experiment,
+                        'series_id': _series_id,
+                        'group': _group,
+                        'length_x': experiments_config.ROI_LENGTH,
+                        'length_y': experiments_config.ROI_HEIGHT,
+                        'length_z': experiments_config.ROI_WIDTH,
+                        'offset_x': _offset_x,
+                        'offset_y': OFFSET_Y,
+                        'offset_z': OFFSET_Z,
+                        'cell_id': 'cell',
+                        'direction': _direction,
+                        'time_point': EXPERIMENTS_TIME_POINT - 1
+                    }
+                    _roi = compute.roi_time_point(_arguments)
+                    _fibers_density = _fibers_densities[_roi]
+
                     if not OUT_OF_BOUNDARIES and _fibers_density[1]:
                         continue
 
@@ -151,7 +157,7 @@ def main():
     _fibers_densities = compute_simulations_fibers_densities(_simulations)
 
     _simulations_fibers_densities = [[] for _i in range(len(OFFSETS_X))]
-    for _simulation in _simulations:
+    for _simulation in tqdm(_simulations, desc='Simulations Loop'):
         _offset_index = 0
         _normalization = simulations_load.normalization(_simulation)
 
