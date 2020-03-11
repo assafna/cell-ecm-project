e970cab624fd1637ddd9a535125d9f3385cb1bf6
Assaf <assafna@post.bgu.ac.il>
2020-03-11 19:17:06 +0200

    can now change x axis too

--- plotting/edit.py
+++ plotting/edit.py
@@ -8,3 +8,15 @@ def update_y_axis(_fig, _range, _color=None, _width=None):
     )
 
     return _fig
+
+
+def update_x_axis(_fig, _range, _color=None, _width=None):
+    _fig.update_layout(
+        xaxis=dict(
+            zerolinecolor=_color,
+            zerolinewidth=_width,
+            range=_range
+        )
+    )
+
+    return _fig
