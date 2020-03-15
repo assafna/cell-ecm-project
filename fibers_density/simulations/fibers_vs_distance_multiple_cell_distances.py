9dfeb31bc24257985c71924b1c5124a3874145a2
Assaf <assafna@post.bgu.ac.il>
2020-03-15 20:48:43 +0200

    only till the middle

--- fibers_density/simulations/fibers_vs_distance_multiple_cell_distances.py
+++ fibers_density/simulations/fibers_vs_distance_multiple_cell_distances.py
@@ -14,6 +14,11 @@ from plotting import scatter, save, update
 TIME_POINT = 50
 CELLS_DISTANCES = [5, 7, 9]
 OFFSET_X_STEP = 0.2
+OFFSET_X_END = {
+    5: 1.6,
+    7: 2.6,
+    9: 3.6
+}
 OFFSET_Y = 0
 
 
@@ -21,16 +26,17 @@ def compute_simulations_fibers_densities(_simulations, _offsets_x):
     _arguments = []
     for _simulation in _simulations:
         for _offset_x in _offsets_x:
-            _arguments.append({
-                'simulation': _simulation,
-                'length_x': ROI_WIDTH,
-                'length_y': ROI_HEIGHT,
-                'offset_x': _offset_x,
-                'offset_y': OFFSET_Y,
-                'cell_id': 'left_cell',
-                'direction': 'inside',
-                'time_point': TIME_POINT
-            })
+            for _cell_id in ['left_cell', 'right_cell']:
+                _arguments.append({
+                    'simulation': _simulation,
+                    'length_x': ROI_WIDTH,
+                    'length_y': ROI_HEIGHT,
+                    'offset_x': _offset_x,
+                    'offset_y': OFFSET_Y,
+                    'cell_id': _cell_id,
+                    'direction': 'inside',
+                    'time_point': TIME_POINT
+                })
 
     _fibers_densities = {}
     with Pool(CPUS_TO_USE) as _p:
@@ -38,7 +44,7 @@ def compute_simulations_fibers_densities(_simulations, _offsets_x):
                 _p.imap_unordered(compute.roi_fibers_density_time_point, _arguments),
                 total=len(_arguments), desc='Computing Rois & Fibers Densities'):
             _fibers_densities[
-                (_keys['simulation'], _keys['offset_x'])] = _value
+                (_keys['simulation'], _keys['offset_x'], _keys['cell_id'])] = _value
         _p.close()
         _p.join()
 
@@ -63,28 +69,24 @@ def main():
         _simulations = filtering.by_distance(_simulations, _distance=_distance)
         _simulations = filtering.by_time_points_amount(_simulations, _time_points=TIME_POINT)
 
-        _offset_x_end = _distance - (TIME_POINT / 100) - ROI_WIDTH
-        _offsets_x = np.arange(start=0, stop=_offset_x_end + OFFSET_X_STEP, step=OFFSET_X_STEP)
-
+        _offsets_x = np.arange(start=0, stop=OFFSET_X_END[_distance] + OFFSET_X_STEP, step=OFFSET_X_STEP)
         _fibers_densities = compute_simulations_fibers_densities(_simulations, _offsets_x)
 
-        _cells_distance_fibers_densities = list([None] * len(_offsets_x))
+        _cells_distance_fibers_densities = [[] for _i in range(len(_offsets_x))]
         for _simulation in _simulations:
             _offset_index = 0
             _normalization = load.normalization(_simulation)
             for _offset_x in _offsets_x:
-                _fibers_density = _fibers_densities[(_simulation, _offset_x)]
+                for _cell_id in ['left_cell', 'right_cell']:
+                    _fibers_density = _fibers_densities[(_simulation, _offset_x, _cell_id)]
 
-                _normalized_fibers_density = compute_lib.z_score(
-                    _fibers_density,
-                    _normalization['average'],
-                    _normalization['std']
-                )
-
-                if _cells_distance_fibers_densities[_offset_index] is None:
-                    _cells_distance_fibers_densities[_offset_index] = [_normalized_fibers_density]
-                else:
+                    _normalized_fibers_density = compute_lib.z_score(
+                        _fibers_density,
+                        _normalization['average'],
+                        _normalization['std']
+                    )
                     _cells_distance_fibers_densities[_offset_index].append(_normalized_fibers_density)
+
                 _offset_index += 1
 
         _x_array.append(_offsets_x)
@@ -103,18 +105,22 @@ def main():
                     'array': [np.std(_array) for _array in _y],
                     'thickness': 1
                 },
-                mode='lines+markers',
-                line={'dash': 'solid'}
+                mode='markers'
             ) for _x, _y, _name in zip(_x_array, _y_array, _names_array)
         ],
         layout={
             'xaxis': {
-                'title': 'Distance from Left Cell (cell size)',
-                'range': [-0.25, 8]
+                'title': 'Distance from Cell (cell size)'
             },
             'yaxis': {
                 'title': 'Fibers Density Z-score',
-                'range': [-1.5, 17]
+                'range': [-1.5, 16]
+            },
+            'legend': {
+                'xanchor': 'right',
+                'yanchor': 'top',
+                'bordercolor': 'black',
+                'borderwidth': 2
             }
         }
     )
