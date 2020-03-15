e4529974dff76d5389b0c4955fa02f76cda4e531
Assaf <assafna@post.bgu.ac.il>
2020-03-15 14:35:00 +0200

    removed error plot

--- plotting/scatter.py
+++ plotting/scatter.py
@@ -1,41 +1,4 @@
-import numpy as np
 import plotly.graph_objs as go
-
-
-def create_error_bars_plot(_x_array, _y_array, _names_array, _modes_array, _dashes_array, _x_axis_title, _y_axis_title,
-                           _title=None):
-    _fig = go.Figure()
-
-    for _x, _y, _name, _mode, _dash in zip(_x_array, _y_array, _names_array, _modes_array, _dashes_array):
-        _averages = []
-        _std = []
-        for _array in _y:
-            _averages.append(np.mean(_array))
-            _std.append(np.std(_array))
-        _fig.add_trace(
-            go.Scatter(
-                y=_averages,
-                x=_x,
-                name=_name,
-                error_y=dict(
-                    type='data',
-                    array=_std,
-                    thickness=1
-                ),
-                mode=_mode,
-                line=dict(
-                    dash=_dash
-                )
-            )
-        )
-
-    _fig.update_layout(
-        title=_title,
-        xaxis_title=_x_axis_title,
-        yaxis_title=_y_axis_title
-    )
-
-    return _fig
 
 
 def create_plot(_x_array, _y_array, _names_array, _modes_array, _show_legend_array, _x_axis_title, _y_axis_title,
