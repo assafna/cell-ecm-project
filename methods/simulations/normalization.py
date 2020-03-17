570c62403ca0dfef1ad313150706eae40dc201f6
Assaf <assafna@post.bgu.ac.il>
2020-03-17 15:34:33 +0200

    fixed usage of ROI dimensions

--- methods/simulations/normalization.py
+++ methods/simulations/normalization.py
@@ -26,24 +26,20 @@ def process_simulation(_simulation, _overwrite=False):
         return
 
     _fibers_densities = []
-    _offsets_x = list(np.arange(OFFSET_X_START, OFFSET_X_END - ROI_WIDTH, STEP))
-    _offsets_y = list(np.arange(OFFSET_Y_START, CELLS_ZONE_Y_START - ROI_HEIGHT, STEP)) +\
-                 list(np.arange(CELLS_ZONE_Y_END, OFFSET_Y_END - ROI_HEIGHT, STEP))
-    _time_point_fibers_densities = load.fibers_densities(_simulation, _time_point=0)
+    _offsets_x = list(np.arange(OFFSET_X_START, OFFSET_X_END - ROI_WIDTH * CELL_DIAMETER, STEP))
+    _offsets_y = list(np.arange(OFFSET_Y_START, CELLS_ZONE_Y_START - ROI_HEIGHT * CELL_DIAMETER, STEP)) + \
+                 list(np.arange(CELLS_ZONE_Y_END, OFFSET_Y_END - ROI_HEIGHT * CELL_DIAMETER, STEP))
 
     # prepare rois
     _rois = []
     for _offset_x in _offsets_x:
         for _offset_y in _offsets_y:
-            _rois.append((_offset_x, _offset_y, _offset_x + ROI_WIDTH, _offset_y + ROI_HEIGHT))
+            _rois.append(
+                (_offset_x, _offset_y, _offset_x + ROI_WIDTH * CELL_DIAMETER, _offset_y + ROI_HEIGHT * CELL_DIAMETER)
+            )
 
     # compute
     _rois_sums = compute.rois_fibers_densities(_simulation, _time_point=0, _rois=_rois)
-
-    # save fibers densities
-    if len(_time_point_fibers_densities) != len(_rois_sums):
-        _time_point_fibers_densities.update(_rois_sums)
-        save.fibers_densities(_simulation, _time_point=0, _fibers_densities=_time_point_fibers_densities)
 
     # save
     _sums = list(_rois_sums.values())
