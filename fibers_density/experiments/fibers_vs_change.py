7681ea2bd260a716b9b93a174901ab10d7155fc5
Assaf <assafna@post.bgu.ac.il>
2020-03-23 12:02:27 +0200

    organized code

--- fibers_density/experiments/fibers_vs_change.py
+++ fibers_density/experiments/fibers_vs_change.py
@@ -1,19 +1,15 @@
 import os
 import sys
-from multiprocessing.pool import Pool
 
 import numpy as np
-import seaborn
-from scipy.stats import stats, pearsonr
 import plotly.graph_objs as go
-from tqdm import tqdm
+from scipy.stats import pearsonr
 
 import libs.compute_lib
 from libs import compute_lib
-from libs.config_lib import CPUS_TO_USE
-from libs.experiments import load, filtering, compute, paths, organize
+from libs.experiments import load, filtering, compute, paths
 from libs.experiments.config import ROI_LENGTH, ROI_WIDTH, ROI_HEIGHT
-from plotting import scatter, save, heatmap
+from plotting import save
 
 EXPERIMENTS = ['SN16', 'SN41']
 MINIMUM_TIME_POINTS = sys.maxsize
@@ -84,55 +80,45 @@ def main():
     _rois_dictionary, _rois_to_compute = compute.rois(_arguments, _keys=['experiment', 'series_id', 'group', 'cell_id'])
     _fibers_densities = compute.fibers_densities(_rois_to_compute)
 
-    _experiments_by_distance = organize.by_cells_distance(_experiments)
-    _fibers_densities_by_distance = {}
-    _change_in_fibers_densities_by_distance = {}
     _heatmap_fibers = []
     _heatmap_fibers_change = []
-    for _distance in _experiments_by_distance:
-        _fibers_densities_array = []
-        _change_in_fibers_densities_array = []
-        for _tuple in _experiments_by_distance[_distance]:
-            print(_tuple)
-            _experiment, _series_id, _group = _tuple
-            _series_normalization = load.normalization_series_file_data(_experiment, 'Series ' + str(_series_id))
-            _series_normalization = [_series_normalization['average'], _series_normalization['std']]
-            for _cell_id in ['left_cell', 'right_cell']:
-                _fibers_densities_by_time = [_fibers_densities[_tuple] for _tuple in
-                                             _rois_dictionary[(_experiment, _series_id, _group, _cell_id)]]
-                _cell_fibers_densities = \
-                    _fibers_densities_by_time[START_TIME_POINT[_experiment]:END_TIME_POINT[_experiment]]
-                _properties = load.group_properties(_experiment, _series_id, _group)
-                _cell_fibers_densities = compute.remove_blacklist(
-                    _experiment, _series_id, _properties['cells_ids'][_cell_id], _cell_fibers_densities)
-                _cell_fibers_densities = compute.longest_fibers_densities_ascending_sequence(_cell_fibers_densities)
+    for _tuple in _experiments:
+        print(_tuple)
+        _experiment, _series_id, _group = _tuple
+        _series_normalization = load.normalization_series_file_data(_experiment, 'Series ' + str(_series_id))
+        _series_normalization = [_series_normalization['average'], _series_normalization['std']]
+        for _cell_id in ['left_cell', 'right_cell']:
+            _fibers_densities_by_time = [_fibers_densities[_tuple] for _tuple in
+                                         _rois_dictionary[(_experiment, _series_id, _group, _cell_id)]]
+            _cell_fibers_densities = \
+                _fibers_densities_by_time[START_TIME_POINT[_experiment]:END_TIME_POINT[_experiment]]
+            _properties = load.group_properties(_experiment, _series_id, _group)
+            _cell_fibers_densities = compute.remove_blacklist(
+                _experiment, _series_id, _properties['cells_ids'][_cell_id], _cell_fibers_densities)
+            _cell_fibers_densities = compute.longest_fibers_densities_ascending_sequence(_cell_fibers_densities)
 
-                # fix if found nan
-                if True in np.isnan(_cell_fibers_densities):
-                    _cell_fibers_densities = _cell_fibers_densities[:np.where(np.isnan(_cell_fibers_densities))[0][0]]
+            # fix if found nan
+            if True in np.isnan(_cell_fibers_densities):
+                _cell_fibers_densities = _cell_fibers_densities[:np.where(np.isnan(_cell_fibers_densities))[0][0]]
 
-                # not enough data
-                if len(_cell_fibers_densities) < DERIVATIVE + 1:
-                    continue
+            # not enough data
+            if len(_cell_fibers_densities) < DERIVATIVE + 1:
+                continue
 
-                _z_score_fibers_density = libs.compute_lib.z_score_fibers_densities_array(
-                    _cell_fibers_densities, _series_normalization
-                )
-                if _experiment in ['SN41', 'SN44']:
-                    for _start_index in [0, 1, 2]:
-                        _fibers_densities_array += _z_score_fibers_density[_start_index::3][DERIVATIVE:]
-                        _change_in_fibers_densities_array += compute_lib.derivative(
-                            _z_score_fibers_density[_start_index::3], _n=DERIVATIVE
-                        )
-                else:
-                    _fibers_densities_array += _z_score_fibers_density[DERIVATIVE:]
-                    _change_in_fibers_densities_array += compute_lib.derivative(
-                        _z_score_fibers_density, _n=DERIVATIVE
+            _z_score_fibers_density = libs.compute_lib.z_score_fibers_densities_array(
+                _cell_fibers_densities, _series_normalization
+            )
+            if _experiment in ['SN41', 'SN44']:
+                for _start_index in [0, 1, 2]:
+                    _heatmap_fibers += _z_score_fibers_density[_start_index::3][DERIVATIVE:]
+                    _heatmap_fibers_change += compute_lib.derivative(
+                        _z_score_fibers_density[_start_index::3], _n=DERIVATIVE
                     )
-        _fibers_densities_by_distance[_distance] = _fibers_densities_array
-        _change_in_fibers_densities_by_distance[_distance] = _change_in_fibers_densities_array
-        _heatmap_fibers += _fibers_densities_array
-        _heatmap_fibers_change += _change_in_fibers_densities_array
+            else:
+                _heatmap_fibers += _z_score_fibers_density[DERIVATIVE:]
+                _heatmap_fibers_change += compute_lib.derivative(
+                    _z_score_fibers_density, _n=DERIVATIVE
+                )
 
     print(pearsonr(_heatmap_fibers, _heatmap_fibers_change))
 
