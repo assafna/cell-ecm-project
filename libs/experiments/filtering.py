13f46b9b4d6e2d6528133f5242c40403a5fa9193
Assaf <assafna@post.bgu.ac.il>
2020-03-31 18:27:29 +0300

    added range control and filter by distance range

--- libs/experiments/filtering.py
+++ libs/experiments/filtering.py
@@ -1,7 +1,7 @@
 from libs.experiments import load, compute, paths
 
 
-def by_distance(_experiments_tuples, _distance, _time_point=0):
+def by_distance(_experiments_tuples, _distance, _time_point=0, _range=1):
     _experiments_tuples_filtered = []
     for _tuple in _experiments_tuples:
         _experiment, _series_id, _group = _tuple
@@ -16,17 +16,38 @@ def by_distance(_experiments_tuples, _distance, _time_point=0):
             _cell_1_coordinates=[(_left_cell_coordinates['x'], _left_cell_coordinates['y'], _left_cell_coordinates['z'])],
             _cell_2_coordinates=[(_right_cell_coordinates['x'], _right_cell_coordinates['y'], _right_cell_coordinates['z'])]
         )
-        if _distance - 0.5 <= _cells_distance <= _distance + 0.5:
+        if _distance - _range <= _cells_distance <= _distance + _range:
             _experiments_tuples_filtered.append(_tuple)
 
     return _experiments_tuples_filtered
 
 
-def by_distances(_experiments_tuples, _distances):
+def by_distance_range(_experiments_tuples, _distance_range, _time_point=0):
+    _experiments_tuples_filtered = []
+    for _tuple in _experiments_tuples:
+        _experiment, _series_id, _group = _tuple
+        _group_properties = load.group_properties(_experiment, _series_id, _group)
+        if len(_group_properties['time_points']) <= _time_point:
+            continue
+        _left_cell_coordinates = _group_properties['time_points'][_time_point]['left_cell']['coordinates']
+        _right_cell_coordinates = _group_properties['time_points'][_time_point]['right_cell']['coordinates']
+        _cells_distance = compute.cells_distance_in_cell_size(
+            _experiment=_experiment,
+            _series_id=_series_id,
+            _cell_1_coordinates=[(_left_cell_coordinates['x'], _left_cell_coordinates['y'], _left_cell_coordinates['z'])],
+            _cell_2_coordinates=[(_right_cell_coordinates['x'], _right_cell_coordinates['y'], _right_cell_coordinates['z'])]
+        )
+        if _distance_range[0] <= _cells_distance <= _distance_range[1]:
+            _experiments_tuples_filtered.append(_tuple)
+
+    return _experiments_tuples_filtered
+
+
+def by_distances(_experiments_tuples, _distances, _range=1):
     # TODO: improve efficiency
     _experiments_tuples_filtered = []
     for _distance in _distances:
-        _distance_tuples = by_distance(_experiments_tuples, _distance)
+        _distance_tuples = by_distance(_experiments_tuples, _distance, _range)
         for _tuple in _distance_tuples:
             if _tuple not in _experiments_tuples_filtered:
                 _experiments_tuples_filtered.append(_tuple)
