1de9ebd5adf88af35272bdad2bcc1770b420e51a
Assaf <assafna@post.bgu.ac.il>
2020-04-02 12:14:59 +0300

    new figure

--- fibers_density/experiments/__init__.py
+++ fibers_density/experiments/__init__.py
@@ -1,8 +1,9 @@
 from fibers_density.experiments import fibers_vs_distance_multiple_cell_distances, communicated_vs_non_communicated, \
-    fibers_vs_change
+    fibers_vs_change, fibers_and_pairs_cells_distance_correlations_vs_distance
 
 __all__ = [
     fibers_vs_distance_multiple_cell_distances,
     communicated_vs_non_communicated,
-    fibers_vs_change
+    fibers_vs_change,
+    fibers_and_pairs_cells_distance_correlations_vs_distance
 ]
