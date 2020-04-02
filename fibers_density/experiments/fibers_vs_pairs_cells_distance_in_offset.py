eadbb5a2261203367572a080bff8d2b3c37056e5
Assaf <assafna@post.bgu.ac.il>
2020-04-02 14:13:29 +0300

    deciding on offset x from outside

--- fibers_density/experiments/fibers_vs_pairs_cells_distance_in_offset.py
+++ fibers_density/experiments/fibers_vs_pairs_cells_distance_in_offset.py
@@ -12,27 +12,24 @@ from plotting import save
 EXPERIMENTS = ['SN16']
 BAND = True
 TIME_POINT = 18
-OFFSET_X = 0
 OFFSET_Y = 0
 OFFSET_Z = 0
 OUT_OF_BOUNDARIES = False
 
 
-def main():
+def main(_offset_x=0):
     _experiments = load.experiments_groups_as_tuples(EXPERIMENTS)
     _experiments = filtering.by_time_points_amount(_experiments, TIME_POINT)
     _experiments = filtering.by_real_cells(_experiments)
     if BAND:
         _experiments = filtering.by_band(_experiments)
 
-    print(len(_experiments))
-
     _arguments = []
     for _tuple in _experiments:
         _experiment, _series_id, _group = _tuple
         _cells_distance = \
             compute.cells_distance_in_cell_size_time_point(_experiment, _series_id, _group, _time_point=TIME_POINT - 1)
-        if _cells_distance / 2 - 0.5 - ROI_LENGTH >= OFFSET_X:
+        if _cells_distance / 2 - 0.5 - ROI_LENGTH >= _offset_x:
             for _cell_id in ['left_cell', 'right_cell']:
                 _arguments.append({
                     'experiment': _experiment,
@@ -41,15 +38,13 @@ def main():
                     'length_x': ROI_LENGTH,
                     'length_y': ROI_HEIGHT,
                     'length_z': ROI_WIDTH,
-                    'offset_x': OFFSET_X,
+                    'offset_x': _offset_x,
                     'offset_y': OFFSET_Y,
                     'offset_z': OFFSET_Z,
                     'cell_id': _cell_id,
                     'direction': 'inside',
                     'time_point': TIME_POINT - 1
                 })
-        else:
-            print(_tuple)
 
     _rois_dictionary, _rois_to_compute = \
         compute.rois(_arguments, _keys=['experiment', 'series_id', 'group', 'cell_id'])
@@ -68,7 +63,6 @@ def main():
                 _fibers_density = _fibers_densities[_roi_tuple]
 
                 if not OUT_OF_BOUNDARIES and _fibers_density[1]:
-                    print('Out of boundaries', _tuple)
                     continue
 
                 _normalized_fibers_density = compute_lib.z_score(
@@ -80,10 +74,7 @@ def main():
                 if not np.isnan(_normalized_fibers_density):
                     _x_array.append(_cells_distance)
                     _y_array.append(_normalized_fibers_density)
-                else:
-                    print('nan', _tuple)
 
-    print(len(_x_array))
     print(pearsonr(_x_array, _y_array))
 
     # plot
@@ -93,24 +84,53 @@ def main():
             y=_y_array,
             mode='markers',
             marker={
-                'size': 15
-            },
-            opacity=0.7
+                'size': 15,
+                'color': 'black'
+            }
         ),
         layout={
             'xaxis': {
-                'title': 'Cells Distance'
+                'title': 'Cells distance (cell diameter)',
+                'zeroline': False
             },
             'yaxis': {
-                'title': 'Fibers Density Z-score'
-            }
+                'title': 'Fibers density z-score',
+                'zeroline': False,
+                'range': [-2.2, 13],
+                'tickmode': 'array',
+                'tickvals': [0, 4, 8, 12]
+            },
+            'shapes': [
+                {
+                    'type': 'line',
+                    'x0': 4.5,
+                    'y0': -2,
+                    'x1': 9.5,
+                    'y1': -2,
+                    'line': {
+                        'color': 'black',
+                        'width': 2
+                    }
+                },
+                {
+                    'type': 'line',
+                    'x0': 4.5,
+                    'y0': -2,
+                    'x1': 4.5,
+                    'y1': 13,
+                    'line': {
+                        'color': 'black',
+                        'width': 2
+                    }
+                }
+            ]
         }
     )
 
     save.to_html(
         _fig=_fig,
         _path=os.path.join(paths.PLOTS, save.get_module_name()),
-        _filename='plot_offset_x_' + str(OFFSET_X)
+        _filename='plot_offset_x_' + str(_offset_x)
     )
 
 
