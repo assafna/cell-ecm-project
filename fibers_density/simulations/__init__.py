81cef62bc7c7cfde7b55dda6f985c54c5587c3bd
Assaf <assafna@post.bgu.ac.il>
2020-04-07 17:11:22 +0300

    new figure

--- fibers_density/simulations/__init__.py
+++ fibers_density/simulations/__init__.py
@@ -1,7 +1,7 @@
 from fibers_density.simulations import fibers_vs_distance_multiple_cell_distances, fibers_vs_derivatives_single_cell, \
     insides_vs_outsides_stds_box, insides_vs_outsides_cell_distances, communicated_vs_non_communicated, \
     communicated_vs_non_communicated_cell_distances, fibers_vs_change, insides_vs_outsides_derivatives, \
-    stationary_vs_fibers_derivatives
+    stationary_vs_fibers_derivatives, fibers_vs_time_derivatives
 
 __all__ = [
     fibers_vs_distance_multiple_cell_distances,
@@ -12,5 +12,6 @@ __all__ = [
     communicated_vs_non_communicated_cell_distances,
     fibers_vs_change,
     insides_vs_outsides_derivatives,
-    stationary_vs_fibers_derivatives
+    stationary_vs_fibers_derivatives,
+    fibers_vs_time_derivatives
 ]
