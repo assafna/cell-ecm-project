425f055f8595919ba91e5d5ff06c449aa8f8437d
Assaf <assafna@post.bgu.ac.il>
2020-04-27 14:55:43 +0300

    switched x and y, new ranges for x and y, new ranges for cells distance,  now with full use of pool, stand alone figure creation, colorbar of experiments

--- fibers_density/experiments/fibers_vs_offsets_in_axes.py
+++ fibers_density/experiments/fibers_vs_offsets_in_axes.py
@@ -3,6 +3,7 @@ from itertools import product
 from multiprocessing.pool import Pool
 
 import numpy as np
+import plotly.graph_objs as go
 import seaborn as sns
 from tqdm import tqdm
 
@@ -10,7 +11,7 @@ from libs import compute_lib
 from libs.config_lib import CPUS_TO_USE
 from libs.experiments import load, filtering, compute, paths
 from libs.experiments.config import ROI_LENGTH, ROI_HEIGHT, ROI_WIDTH
-from plotting import heatmap, save
+from plotting import save
 
 EXPERIMENTS = ['SN16']
 TIME_POINT = 18
@@ -18,17 +19,19 @@ EXPERIMENTS_STR = '_'.join(EXPERIMENTS)
 REAL_CELLS = True
 STATIC = False
 BAND = True
-DOMAIN_ABSOLUTE_VALUE = 5
-VALUES_BY_CELL_DIAMETER = [
-    round(_value, 10) for _value in
-    np.arange(start=0, stop=DOMAIN_ABSOLUTE_VALUE * 2 + 0.1, step=0.1) - DOMAIN_ABSOLUTE_VALUE]
 OFFSET_X = 0
-CELLS_DISTANCES = [6, 7, 8, 9]
+OFFSET_Y_START = -2.3
+OFFSET_Y_END = 3.9
+OFFSET_Y_STEP = 0.1
+OFFSET_Z_START = -5
+OFFSET_Z_END = 5
+OFFSET_Z_STEP = 0.1
+CELLS_DISTANCE_RANGE = [4, 10]
 DIRECTION = 'inside'
 
 # globals
 _experiments = []
-_experiments_fibers_densities = None
+_experiments_fibers_densities = {}
 _z_array = None
 
 
@@ -54,9 +57,9 @@ def compute_data(_arguments):
             _fibers_densities_array.append(_normalized_fibers_density)
 
     if len(_fibers_densities_array) > 0:
-        return _offset_z_index, _offset_y_index, np.mean(_fibers_densities_array)
+        return _offset_y_index, _offset_z_index, np.mean(_fibers_densities_array)
     else:
-        return _offset_z_index, _offset_y_index, None
+        return _offset_y_index, _offset_z_index, None
 
 
 def main():
@@ -64,17 +67,19 @@ def main():
 
     _experiments = load.experiments_groups_as_tuples(EXPERIMENTS)
     _experiments = filtering.by_time_points_amount(_experiments, TIME_POINT)
-    _experiments = filtering.by_distances(_experiments, CELLS_DISTANCES)
+    _experiments = filtering.by_distance_range(_experiments, CELLS_DISTANCE_RANGE)
     _experiments = filtering.by_real_cells(_experiments, _real_cells=REAL_CELLS)
     _experiments = filtering.by_static_cells(_experiments, _static=STATIC)
     if BAND:
         _experiments = filtering.by_band(_experiments)
+    print('Total experiments:', len(_experiments))
 
+    _offsets_y = np.arange(start=OFFSET_Y_START, stop=OFFSET_Y_END + OFFSET_Y_STEP, step=OFFSET_Y_STEP)
+    _offsets_z = np.arange(start=OFFSET_Z_START, stop=OFFSET_Z_END + OFFSET_Z_STEP, step=OFFSET_Z_STEP)
     _arguments = []
     for _tuple in _experiments:
         _experiment, _series_id, _group = _tuple
-        for _offset_y, _offset_z, _cell_id in \
-                product(VALUES_BY_CELL_DIAMETER, VALUES_BY_CELL_DIAMETER, ['left_cell', 'right_cell']):
+        for _offset_y, _offset_z, _cell_id in product(_offsets_y, _offsets_z, ['left_cell', 'right_cell']):
             _arguments.append({
                 'experiment': _experiment,
                 'series_id': _series_id,
@@ -105,36 +110,46 @@ def main():
 
     _arguments = []
     for (_offset_y_index, _offset_y), (_offset_z_index, _offset_z) in \
-            product(enumerate(VALUES_BY_CELL_DIAMETER), enumerate(VALUES_BY_CELL_DIAMETER)):
+            product(enumerate(_offsets_y), enumerate(_offsets_z)):
         _arguments.append(
             (_offset_y_index, _offset_y, _offset_z_index, _offset_z))
 
-    _z_array = np.zeros(shape=(len(VALUES_BY_CELL_DIAMETER), len(VALUES_BY_CELL_DIAMETER)))
-    with Pool(1) as _p:
+    _z_array = np.zeros(shape=(len(_offsets_y), len(_offsets_z)))
+    with Pool(CPUS_TO_USE) as _p:
         for _answer in tqdm(_p.imap_unordered(compute_data, _arguments), total=len(_arguments),
                             desc='Computing Heatmap'):
-            _offset_z_index, _offset_y_index, _mean = _answer
-            _z_array[_offset_z_index, _offset_y_index] = _mean
+            _offset_y_index, _offset_z_index, _mean = _answer
+            _z_array[_offset_y_index, _offset_z_index] = _mean
         _p.close()
         _p.join()
 
-    # plot
-    _fig = heatmap.create_plot(
-        _x_labels=VALUES_BY_CELL_DIAMETER,
-        _y_labels=VALUES_BY_CELL_DIAMETER,
-        _z_array=_z_array,
-        _x_axis_title='Offset in Z axis',
-        _y_axis_title='Offset in XY axis',
-        _color_scale=sns.color_palette('BrBG').as_hex(),
-        _zmin=-6,
-        _zmax=6
+    _fig = go.Figure(
+        data=go.Heatmap(
+            x=_offsets_z,
+            y=_offsets_y,
+            z=_z_array,
+            colorscale=sns.light_palette('orange').as_hex(),
+            showscale=True,
+            zmin=-3,
+            zmax=8
+        ),
+        layout={
+            'xaxis': {
+                'title': 'Offset in XY axis (cell diameter)',
+                'zeroline': False
+            },
+            'yaxis': {
+                'title': 'Offset in Z axis (cell diameter)',
+                'zeroline': False
+            }
+        }
     )
 
     save.to_html(
         _fig=_fig,
         _path=os.path.join(paths.PLOTS, save.get_module_name()),
         _filename='plot_' + EXPERIMENTS_STR + '_real_' + str(REAL_CELLS) + '_static_' + str(STATIC) + '_band_' +
-                  str(BAND) + '_heatmap'
+                  str(BAND)
     )
 
 
