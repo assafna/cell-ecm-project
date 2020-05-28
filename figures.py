82196945d01263c4cfab7a4b711b587630fab757
Assaf <assafna@post.bgu.ac.il>
2020-05-28 18:04:01 +0300

    divided to figures

--- figures.py
+++ figures.py
@@ -1,7 +1,7 @@
 import fibers_density
 
 
-def figure_1():
+def figure_1_main():
     print('\nPanel D')
     fibers_density.fibers_vs_distance_single_cells.main(_low_connectivity=False)
 
@@ -26,7 +26,16 @@ def figure_si_1_correlation_distance_density_offset():
     fibers_density.experiments.fibers_vs_pairs_cells_distance_in_offset.main()
 
 
-def figure_2():
+def figure_1_all():
+    print('\nFigure 1 - Main Figure')
+    figure_1_main()
+
+    print('\nFigure SI 1')
+    print('\nFigure SI 1 - Correlation Distance Density Offset')
+    figure_si_1_correlation_distance_density_offset()
+
+
+def figure_2_main():
     print('\nPanel B')
     fibers_density.fibers_in_time_single_cells.main()
 
@@ -44,7 +53,16 @@ def figure_si_2_density_vs_change_in_density_in_late_time_points():
     fibers_density.experiments.fibers_vs_change.main(_early_time_points=False)
 
 
-def figure_3():
+def figure_2_all():
+    print('\nFigure 2 - Main Figure')
+    figure_2_main()
+
+    print('\nFigure SI 2')
+    print('\nFigure SI 2 - Density vs. Change in Density in Late Time-points')
+    figure_si_2_density_vs_change_in_density_in_late_time_points()
+
+
+def figure_3_main():
     print('\nPanels B & C')
     fibers_density.simulations.correlations_by_derivatives_pairs.main()
 
@@ -78,7 +96,25 @@ def figure_si_3_insides_vs_outsides_offsets():
     fibers_density.simulations.insides_vs_outsides_offsets.main()
 
 
-def figure_4():
+def figure_3_all():
+    print('\nFigure 3 - Main Figure')
+    figure_3_main()
+
+    print('\nFigure SI 3')
+    print('\nFigure SI 3 - Stationary & Detrending')
+    figure_si_3_stationary_and_detrending()
+
+    print('\nFigure SI 3 - Single Cell Detrending')
+    figure_si_3_single_cell_detrending()
+
+    print('\nFigure SI 3 - Insides vs. Outsides Distances')
+    figure_si_3_insides_vs_outsides_distances()
+
+    print('\nFigure SI 3 - Insides vs. Outsides Offsets')
+    figure_si_3_insides_vs_outsides_offsets()
+
+
+def figure_4_main():
     print('\nPanel B')
     fibers_density.simulations.same_vs_different.main(_low_connectivity=False)
 
@@ -175,7 +211,34 @@ def figure_si_4_temporal_assessment():
     fibers_density.experiments.same_vs_different_temporal_assessment.main(_high_time_resolution=True)
 
 
-def figure_5():
+def figure_4_all():
+    print('\nFigure 4 - Main Figure')
+    figure_4_main()
+
+    print('\nFigure SI 4')
+    print('\nFigure SI 4 - No Band')
+    figure_si_4_no_band()
+
+    print('\nFigure SI 4 - Single Cell Detrending')
+    figure_si_4_single_cell_detrending()
+
+    print('\nFigure SI 4 - Same vs. Different - Distances')
+    figure_si_4_same_vs_diff_distances()
+
+    print('\nFigure SI 4 - Same vs. Different - Controls')
+    figure_si_4_same_vs_diff_controls()
+
+    print('\nFigure SI 4 - Matchmaking')
+    figure_si_4_matchmaking()
+
+    print('\nFigure SI 4 - Same vs. Different - High Temporal Resolution')
+    figure_si_4_same_vs_diff_high_time_res()
+
+    print('\nFigure SI 4 - Temporal Assessment')
+    figure_si_4_temporal_assessment()
+
+
+def figure_5_main():
     print('\nPanel B')
     fibers_density.experiments.fibers_vs_offsets_in_axes.main(_high_time_resolution=False)
 
@@ -225,7 +288,28 @@ def figure_si_5_communication_vs_density():
     fibers_density.experiments.fibers_vs_same_vs_different_offsets_in_axes.main(_band=True, _high_time_resolution=False)
 
 
-def figure_6():
+def figure_5_all():
+    print('\nFigure 5 - Main Figure')
+    figure_5_main()
+
+    print('\nFigure SI 5')
+    print('\nFigure SI 5 - High Temporal Resolution')
+    figure_si_5_high_time_res()
+
+    print('\nFigure SI 5 - Simulations')
+    figure_si_5_simulations()
+
+    print('\nFigure SI 5 - No Band')
+    figure_si_5_no_band()
+
+    print('\nFigure SI 5 - Real vs. Fake')
+    figure_si_5_real_vs_fake()
+
+    print('\nFigure SI 5 - Communication vs. Density')
+    figure_si_5_communication_vs_density()
+
+
+def figure_6_main():
     print('\nPanels B & C')
     fibers_density.simulations.same_vs_different_cross_correlation.main(
         _alpha=1, _beta=1, _low_connectivity=False, _plots=['same'])
@@ -260,87 +344,11 @@ def figure_si_6_granger_causality():
         _band=None, _high_time_resolution=True, _tuples_to_plot=[('SN45', 2, 'cells_2_3')])
 
 
-def figure_si_discussion_saturation():
-    print('\nPanel A')
-    fibers_density.experiments.inner_regions_saturation.main(_offset_y=0)
+def figure_6_all():
+    print('\nFigure 6 - Main Figure')
+    figure_6_main()
 
-    print('\nPanel B')
-    fibers_density.experiments.inner_regions_saturation.main(_offset_y=0.5)
-
-
-if __name__ == '__main__':
-    print('\nFigure 1')
-    figure_1()
-
-    print('\nFigure SI 1 - Correlation Distance Density Offset')
-    figure_si_1_correlation_distance_density_offset()
-
-    print('\nFigure 2')
-    figure_2()
-
-    print('\nFigure SI 2 - Density vs. Change in Density in Late Time-points')
-    figure_si_2_density_vs_change_in_density_in_late_time_points()
-
-    print('\nFigure 3')
-    figure_3()
-
-    print('\nFigure SI 3 - Stationary & Detrending')
-    figure_si_3_stationary_and_detrending()
-
-    print('\nFigure SI 3 - Single Cell Detrending')
-    figure_si_3_single_cell_detrending()
-
-    print('\nFigure SI 3 - Insides vs. Outsides Distances')
-    figure_si_3_insides_vs_outsides_distances()
-
-    print('\nFigure SI 3 - Insides vs. Outsides Offsets')
-    figure_si_3_insides_vs_outsides_offsets()
-
-    print('\nFigure 4')
-    figure_4()
-
-    print('\nFigure SI 4 - No Band')
-    figure_si_4_no_band()
-
-    print('\nFigure SI 4 - Single Cell Detrending')
-    figure_si_4_single_cell_detrending()
-
-    print('\nFigure SI 4 - Same vs. Different - Distances')
-    figure_si_4_same_vs_diff_distances()
-
-    print('\nFigure SI 4 - Same vs. Different - Controls')
-    figure_si_4_same_vs_diff_controls()
-
-    print('\nFigure SI 4 - Matchmaking')
-    figure_si_4_matchmaking()
-
-    print('\nFigure SI 4 - Same vs. Different - High Temporal Resolution')
-    figure_si_4_same_vs_diff_high_time_res()
-
-    print('\nFigure SI 4 - Temporal Assessment')
-    figure_si_4_temporal_assessment()
-
-    print('\nFigure 5')
-    figure_5()
-
-    print('\nFigure SI 5 - High Temporal Resolution')
-    figure_si_5_high_time_res()
-
-    print('\nFigure SI 5 - Simulations')
-    figure_si_5_simulations()
-
-    print('\nFigure SI 5 - No Band')
-    figure_si_5_no_band()
-
-    print('\nFigure SI 5 - Real vs. Fake')
-    figure_si_5_real_vs_fake()
-
-    print('\nFigure SI 5 - Communication vs. Density')
-    figure_si_5_communication_vs_density()
-
-    print('\nFigure 6')
-    figure_6()
-
+    print('\nFigure SI 6')
     print('\nFigure SI 6 - Betas')
     figure_si_6_betas()
 
@@ -350,5 +358,26 @@ if __name__ == '__main__':
     print('\nFigure SI 6 - Granger Causality')
     figure_si_6_granger_causality()
 
+
+def figure_si_discussion_saturation():
+    print('\nPanel A')
+    fibers_density.experiments.inner_regions_saturation.main(_offset_y=0)
+
+    print('\nPanel B')
+    fibers_density.experiments.inner_regions_saturation.main(_offset_y=0.5)
+
+
+def figure_discussion_all():
     print('\nFigure SI Discussion - Saturation')
     figure_si_discussion_saturation()
+
+
+if __name__ == '__main__':
+    print('\nFigures')
+    figure_1_all()
+    figure_2_all()
+    figure_3_all()
+    figure_4_all()
+    figure_5_all()
+    figure_6_all()
+    figure_discussion_all()
