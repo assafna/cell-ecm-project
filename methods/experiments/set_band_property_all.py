07000bee22f44018cb786b928df7f0b6c5e808e5
Assaf <assafna@post.bgu.ac.il>
2020-11-30 13:07:53 +0200

    setting all band properties to the same value

---
+++ methods/experiments/set_band_property_all.py
@@ -0,0 +1,26 @@
+import os
+
+from libs import save_lib
+from libs.experiments import load, paths
+
+
+def main():
+    _experiment = 'Bleb_150'
+    _band = False
+
+    _tuples = load.experiment_groups_as_tuples(_experiment)
+    print('Total tuples:', len(_tuples))
+
+    for _tuple in _tuples:
+        print(_tuple)
+        _experiment, _series_id, _group = _tuple
+        _properties = load.group_properties(_experiment, _series_id, _group)
+        _properties['band'] = _band
+
+        _group_structured_path = paths.structured(_experiment, _series_id, _group)
+        _properties_json_path = os.path.join(_group_structured_path, 'properties.json')
+        save_lib.to_json(_properties, _properties_json_path)
+
+
+if __name__ == '__main__':
+    main()
