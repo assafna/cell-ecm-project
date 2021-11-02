9dea3fb727b32eea945a7902aa4111821cefece3
assaf <assafna@post.bgu.ac.il>
2021-11-02 19:08:00 +0200

    new panel

--- fiber_density/simulations/__init__.py
+++ fiber_density/simulations/__init__.py
@@ -11,7 +11,8 @@ from fiber_density.simulations import inner_density_vs_window_distance, \
     inner_density_vs_window_distance_with_single_cells, \
     communicating_inner_correlation_vs_non_communicating_inner_correlation, inner_density_vs_time_cell_pairs, \
     inner_correlation_communicating_vs_non_communicating_vs_derivatives, \
-    inner_correlation_communicating_vs_non_communicating_vs_pair_distance
+    inner_correlation_communicating_vs_non_communicating_vs_pair_distance, \
+    inner_correlation_communicating_vs_non_communicating_vs_window_distance
 
 __all__ = [
     inner_density_vs_window_distance,
@@ -34,5 +35,6 @@ __all__ = [
     communicating_inner_correlation_vs_non_communicating_inner_correlation,
     inner_density_vs_time_cell_pairs,
     inner_correlation_communicating_vs_non_communicating_vs_derivatives,
-    inner_correlation_communicating_vs_non_communicating_vs_pair_distance
+    inner_correlation_communicating_vs_non_communicating_vs_pair_distance,
+    inner_correlation_communicating_vs_non_communicating_vs_window_distance
 ]
--- figures.py
+++ figures.py
@@ -16,6 +16,9 @@ def figure_si_1():
     print('\nPanel C')
     fiber_density.simulations.inner_correlation_communicating_vs_non_communicating_vs_pair_distance.main()
 
+    print('\nPanel D')
+    fiber_density.simulations.inner_correlation_communicating_vs_non_communicating_vs_window_distance.main()
+
 
 # def figure_si_1_distances():
 #     print('\nPanel A')
