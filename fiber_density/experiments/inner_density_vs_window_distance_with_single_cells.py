c02d03ad4deb50e2627d447ba614cdafb4676622
assaf <assafna@post.bgu.ac.il>
2021-11-05 16:25:14 +0200

    first version

---
+++ fiber_density/experiments/inner_density_vs_window_distance_with_single_cells.py
@@ -0,0 +1,111 @@
+import os
+
+import numpy as np
+import plotly.graph_objs as go
+
+from fiber_density import inner_density_vs_window_distance_single_cells
+from fiber_density.experiments import inner_density_vs_window_distance
+from libs.experiments import config, paths
+from plotting import save
+
+
+def main():
+    print('Single cells')
+    _single_cells_fiber_densities = inner_density_vs_window_distance_single_cells.compute_experiments_data()
+
+    print('Cell pairs')
+    _names_array, _x_array, _y_array = inner_density_vs_window_distance.compute_cell_pairs()
+
+    # plot
+    _colors_array = config.colors(len(_names_array) + 1)
+    _fig = go.Figure(
+        data=[
+                 go.Scatter(
+                     x=inner_density_vs_window_distance_single_cells.OFFSETS_X,
+                     y=[np.mean(_array) for _array in _single_cells_fiber_densities],
+                     name='Single cells',
+                     error_y={
+                         'type': 'data',
+                         'array': [np.std(_array) for _array in _single_cells_fiber_densities],
+                         'thickness': 1,
+                         'color': _colors_array[0]
+                     },
+                     mode='markers',
+                     marker={
+                         'size': 15,
+                         'color': _colors_array[0]
+                     },
+                     opacity=0.7
+                 )] + [
+                 go.Scatter(
+                     x=_x,
+                     y=[np.mean(_array) for _array in _y],
+                     name=_name,
+                     error_y={
+                         'type': 'data',
+                         'array': [np.std(_array) for _array in _y],
+                         'thickness': 1,
+                         'color': _color
+                     },
+                     mode='markers',
+                     marker={
+                         'size': 15,
+                         'color': _color
+                     },
+                     opacity=0.7
+                 ) for _x, _y, _name, _color in zip(_x_array, _y_array, _names_array, _colors_array[1:])
+             ],
+        layout={
+            'xaxis': {
+                'title': 'Window distance (cell diameter)',
+                'zeroline': False
+            },
+            'yaxis': {
+                'title': 'Fiber density (z-score)',
+                'range': [-1.7, 13],
+                'zeroline': False,
+                'tickmode': 'array',
+                'tickvals': [0, 4, 8, 12]
+            },
+            'legend': {
+                'xanchor': 'right',
+                'yanchor': 'top',
+                'bordercolor': 'black',
+                'borderwidth': 2
+            },
+            'shapes': [
+                {
+                    'type': 'line',
+                    'x0': -0.2,
+                    'y0': -1.5,
+                    'x1': 3.4,
+                    'y1': -1.5,
+                    'line': {
+                        'color': 'black',
+                        'width': 2
+                    }
+                },
+                {
+                    'type': 'line',
+                    'x0': -0.2,
+                    'y0': -1.5,
+                    'x1': -0.2,
+                    'y1': 13,
+                    'line': {
+                        'color': 'black',
+                        'width': 2
+                    }
+                }
+            ]
+        }
+    )
+
+    save.to_html(
+        _fig=_fig,
+        _path=os.path.join(paths.PLOTS, save.get_module_name()),
+        _filename='plot'
+    )
+
+
+if __name__ == '__main__':
+    main()
