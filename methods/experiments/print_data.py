8562261f74cc7f6862595f6a4d903c44dd12bae0
assaf <assafna@post.bgu.ac.il>
2022-02-04 22:40:12 +0200

    grouped by columns before printing

--- methods/experiments/print_data.py
+++ methods/experiments/print_data.py
@@ -1,3 +1,6 @@
+import pandas as pd
+from tqdm import tqdm
+
 from libs.experiments import compute, load, organize, filtering, config
 
 SINGLE_CELLS = ['Single_Cell_Ortal']
@@ -46,15 +49,15 @@ def get_pair_distance_range(_tuple):
     try:
         _distance = compute.pair_distance_in_cell_size_time_frame(_experiment, _series_id, _group, _time_frame=0)
         if _distance < 4:
-            return '<4'
+            return 'Less than 4'
         elif 4 <= _distance < 6:
-            return '4-6'
+            return 'Between 4 and 6'
         elif 6 <= _distance < 8:
-            return '6-8'
+            return 'Between 6 and 8'
         elif 8 <= _distance < 10:
-            return '8-10'
+            return 'Between 8 and 10'
         else:
-            return '>10'
+            return 'More than 10'
     except IndexError:
         return ''
 
@@ -76,8 +79,15 @@ def is_dead_cells_only(_tuple):
     return _experiment in CELL_PAIRS_DEAD_ONLY
 
 
+def print_df(_df):
+    print('\t'.join(map(str, _df.columns)))
+    for _i in range(len(_df.index)):
+        _row = list(_df.iloc[_i])
+        print('\t'.join(map(str, _row)))
+
+
 def main():
-    print(
+    _headers = [
         'Single cell / cell pair',
         'Regular / bleb / with dead cells / beads / metastasis',
         'Is real?',
@@ -87,35 +97,34 @@ def main():
         'Bleb from the beginning?',
         'Bleb amount',
         'is dead cells only?',
-        'N',
-        sep='\t'
-    )
+        'N'
+    ]
 
     _rows = []
 
     # single cells
+    print('Single cells')
     _single_cells = load.experiments_groups_as_tuples(SINGLE_CELLS)
     _single_cells = filtering.by_main_cell(_single_cells)
     _single_cells = organize.by_single_cell_id(_single_cells)
-    print(
+    _rows.append([
         'Single cell',
         'Regular',
-        'Real',
+        True,
         '',
         compute.temporal_resolution_in_minutes(SINGLE_CELLS[0]),
         '',
         '',
         '',
         '',
-        len(_single_cells),
-        sep='\t'
-    )
+        len(_single_cells)
+    ])
 
     # cell pairs
     _cell_pairs = load.experiments_groups_as_tuples(CELL_PAIRS)
-    for _tuple in _cell_pairs:
+    for _tuple in tqdm(_cell_pairs, desc='Cell pairs'):
         _experiment, _series_id, _group = _tuple
-        print(
+        _rows.append([
             'Cell pair',
             get_type(_tuple),
             filtering.is_real(_tuple),
@@ -125,9 +134,26 @@ def main():
             is_bleb_from_start(_tuple),
             bleb_amount(_tuple),
             is_dead_cells_only(_tuple),
-            1,
-            sep='\t'
-        )
+            1
+        ])
+
+    _df = pd.DataFrame(data=_rows, columns=_headers)
+
+    # summarize
+    _summarize = _df.groupby(by=[
+        'Single cell / cell pair',
+        'Regular / bleb / with dead cells / beads / metastasis',
+        'Is real?',
+        'Is band?',
+        'Temporal resolution',
+        'Pair distance',
+        'Bleb from the beginning?',
+        'Bleb amount',
+        'is dead cells only?'
+    ]).sum()
+
+    # print(_summarize.reset_index())
+    print_df(_summarize.reset_index())
 
 
 if __name__ == '__main__':
